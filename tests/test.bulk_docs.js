c508e7e79b0413d45884ab5b7c0ad6401bead806
Dale Harvey <dale@arandomurl.com>
2012-03-11 18:07:46 +0000

    Add to bulk docs testing

--- tests/test.bulk_docs.js
+++ tests/test.bulk_docs.js
@@ -21,9 +21,20 @@ asyncTest('Testing bulk docs', function() {
         for (i = 0; i < 5; i++) {
           ok(results[i].id == i.toString(), 'id matches again');
           // set the delete flag to delete the docs in the next step
+          docs[i]._rev = results[i].rev;
           docs[i]._deleted = true;
         }
-        start();
+        db.put(docs[0], function(err, doc) {
+          db.bulkDocs({docs: docs}, function(err, results) {
+            ok(results[0].error === 'conflict', 'First doc should be in conflict');
+            ok(typeof results[0].rev === "undefined", 'no rev in conflict');
+            for (i = 1; i < 5; i++) {
+              ok(results[i].id == i.toString());
+              ok(results[i].rev);
+            }
+            start();
+          });
+        });
       });
     });
   });
