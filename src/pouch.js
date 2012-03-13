836092691a0197d71a3f678892d66ff913cfc24a
Dale Harvey <dale@arandomurl.com>
2012-03-13 22:07:43 +0000

    getTime isnt necessarily unique, uuid should be

--- src/pouch.js
+++ src/pouch.js
@@ -332,7 +332,7 @@
       };
 
       var writeDoc = function(docInfo, callback) {
-        docInfo.data._junk = new Date().getTime();
+        docInfo.data._junk = Math.uuid();
         var dataReq = txn.objectStore(BY_SEQ_STORE).put(docInfo.data);
         dataReq.onsuccess = function(e) {
           docInfo.metadata.seq = e.target.result;
