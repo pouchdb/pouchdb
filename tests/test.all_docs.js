f66c195e35cd2d6094b09b450dc70d411d8d8dea
Dale Harvey <dale@arandomurl.com>
2012-03-15 22:17:34 +0000

    Write documents in tests sequentially so we can test changes order

--- tests/test.all_docs.js
+++ tests/test.all_docs.js
@@ -13,17 +13,29 @@ module('all_docs', {
 });
 
 asyncTest('Testing all docs', function() {
-  initTestDB(this.name, function(err, db) {
-    var docs = [
-      {_id:"0",a:1,b:1},
-      {_id:"3",a:4,b:16},
-      {_id:"1",a:2,b:4},
-      {_id:"2",a:3,b:9}
-    ];
-    db.bulkDocs({docs: docs}, function(err, result) {
-      result.forEach(function(doc) {
-        ok(doc.ok, 'doc write returned ok');
-      });
+
+  var db;
+  var docs = [
+    {_id:"0",a:1,b:1},
+    {_id:"3",a:4,b:16},
+    {_id:"1",a:2,b:4},
+    {_id:"2",a:3,b:9}
+  ];
+
+  function writeDocs(callback) {
+    if (!docs.length) {
+      return callback();
+    }
+    var doc = docs.shift();
+    db.put(doc, function(err, doc) {
+      ok(doc.ok, 'docwrite returned ok');
+      writeDocs(callback);
+    });
+  };
+
+  initTestDB(this.name, function(err, _db) {
+    db = _db;
+    writeDocs(function() {
       db.allDocs(function(err, result) {
         var rows = result.rows;
         ok(result.total_rows === 4, 'correct number of results');
