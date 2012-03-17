f77063a46e1d8dce8ff021d9e91bc6a0ca8b86a4
Dale Harvey <dale@arandomurl.com>
2012-03-17 03:01:54 +0000

    use deepEqual to compare objects

--- tests/test.merge_rev_tree.js
+++ tests/test.merge_rev_tree.js
@@ -4,6 +4,9 @@
 module('merge rev tree');
 
 function merge(tree1, tree2, depth) {
+  if (!tree1.length) {
+    return {tree: [tree2], type: 'new_leaf'};
+  }
   return false;
 }
 
@@ -33,77 +36,77 @@ test('Test merging revision tree', function() {
                         {pos: 2, ids: ['2_1']},
                         {pos: 3, ids: ['3_1']}];
 
-  equal(merge([], simple, 10), {
+  deepEqual(merge([], simple, 10), {
     tree: [simple],
     type: 'new_leaf'
   }, 'Merging a path into an empty tree is the path');
 
-  equal(merge([simple], simple, 10), {
+  deepEqual(merge([simple], simple, 10), {
     tree: [simple],
     type: 'internal_node'
   }, 'Remerge path into path is reflexive');
 
-  equal(merge([], two0, 10), {
+  deepEqual(merge([], two0, 10), {
     tree: [two0],
     type: 'new_leaf'
   }, 'Merging a path with multiple entries is the path');
 
-  equal(merge([two0], two0, 10), {
+  deepEqual(merge([two0], two0, 10), {
     tree: [two0],
     type: 'internal_node'
   }, 'Merging a path with multiple entries is reflexive');
 
-  equal(merge([two0], simple, 10), {
+  deepEqual(merge([two0], simple, 10), {
     tree: [two0],
     type: 'internal_node'
   }, 'Merging a subpath into a path results in the path');
 
-  equal(merge([two0], newleaf, 10), {
+  deepEqual(merge([two0], newleaf, 10), {
     tree: [withnewleaf],
     type: 'new_leaf'
   }, 'Merging a new leaf gives us a new leaf');
 
-  equal(merge([two0], two1, 10), {
+  deepEqual(merge([two0], two1, 10), {
     tree: [newbranch],
     type: 'new_branch'
   }, 'Merging a new branch returns a proper tree');
 
-  equal(merge([two1], two0, 10), {
+  deepEqual(merge([two1], two0, 10), {
     tree: [newbranch],
     type: 'new_branch'
   }, 'Order of merging does not affect the resulting tree');
 
-  equal(merge([newbranch], newleaf, 10), {
+  deepEqual(merge([newbranch], newleaf, 10), {
     tree: [newbranchleaf],
     type: 'new_leaf'
   }, 'Merging a new_leaf doesnt return new_branch when branches exist');
 
-  equal(merge([newbranchleaf], newdeepbranch, 10), {
+  deepEqual(merge([newbranchleaf], newdeepbranch, 10), {
     tree: [newbranchleafbranch],
     type: 'new_branch'
   }, 'Merging a deep branch with branches works');
 
-  equal(merge(stemmedconflicts, withnewleaf, 10), {
+  deepEqual(merge(stemmedconflicts, withnewleaf, 10), {
     tree: [withnewleaf],
     type: 'new_leaf'
   }, 'Merging a deep branch with branches works');
 
-  equal(merge([two0], newleaf, 2), {
+  deepEqual(merge([two0], newleaf, 2), {
     tree: [newleaf],
     type: 'new_leaf'
   }, 'Simple stemming works');
 
-  equal(merge([newbranchleafbranch], simple, 2), {
+  deepEqual(merge([newbranchleafbranch], simple, 2), {
     tree: stemmed2,
     type: 'internal_node'
   }, 'Merge with stemming works correctly for branches');
 
-  equal(merge([newbranchleafbranch], simple, 1), {
+  deepEqual(merge([newbranchleafbranch], simple, 1), {
     tree: stemmed3,
     type: 'internal_node'
   }, 'Merge with stemming to leaves works fine');
 
-  equal(merge(stemmed3, withnewleaf, 10), {
+  deepEqual(merge(stemmed3, withnewleaf, 10), {
     tree: partialrecover,
     type: 'internal_node'
   }, 'Merging unstemmed recovers as much as possible without losing info');
