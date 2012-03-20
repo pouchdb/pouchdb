d94923df764dcfad1e13708a34d8d328bfbb4266
Dale Harvey <dale@arandomurl.com>
2012-03-20 15:24:24 +0000

    minor refactorings

--- tests/test.merge_rev_tree.js
+++ tests/test.merge_rev_tree.js
@@ -3,54 +3,68 @@
 
 module('merge rev tree');
 
-function actuallyExpandTree(all, current, pos, arr) {
-  current = current.slice(0);
-  current.push(arr[0]);
-  if (!arr[1].length) {
-    all.push({pos: pos, ids: current});
-  }
-  for (var i = 0; i < arr[1].length; i++) {
-    var res = actuallyExpandTree(all, current, pos, arr[1][i]);
-  }
-}
+// for a better overview of what this is doing, read:
+// https://github.com/apache/couchdb/blob/master/src/couchdb/couch_key_tree.erl
+//
+// But for a quick intro, CouchDB uses a revision tree to store a documents
+// history, A -> B -> C, when a document has conflicts, that is a branch in the
+// tree, A -> (B1 | B2 -> C), We store these as a nested array in the format
+//
+// KeyTree = [Path ... ]
+// Path = {pos: position_from_root, ids: Tree}
+// Tree = [Key, Tree]
 
+// Turn a path as a flat array into a tree with a single branch
 function pathToTree(path) {
-  var first = path.shift();
-  var root = [first, []]
+  var root = [path.shift(), []];
   var leaf = root;
   while (path.length) {
-    var el = path.shift();
-    nleaf = [el, []];
+    nleaf = [path.shift(), []];
     leaf[1].push(nleaf);
     leaf = nleaf;
   }
   return root;
 }
 
-function expandTree(tree) {
-  var all = [];
-  for (var i = 0; i < tree.length; i++) {
-    actuallyExpandTree(all, [], tree[i].pos, tree[i].ids);
+// Turn a tree into a list of rootToLeaf paths
+function expandTree(all, current, pos, arr) {
+  current = current.slice(0);
+  current.push(arr[0]);
+  if (!arr[1].length) {
+    all.push({pos: pos, ids: current});
   }
+  arr[1].forEach(function(child) {
+    expandTree(all, current, pos, child);
+  });
+}
+
+function rootToLeaf(tree) {
+  var all = [];
+  tree.forEach(function(path) {
+    expandTree(all, [], path.pos, path.ids);
+  });
   return all;
 }
 
+// To ensure we dont grow the revision tree infinitely, we stem old revisions
 function stem(tree, depth) {
-  var stemmedPaths = expandTree(tree).map(function(path) {
-    var npath = path.ids.slice(-depth);
-    var olength = npath.length;
-    var ids = pathToTree(npath);
+  // First we break out the tree into a complete list of root to leaf paths,
+  // we cut off the start of the path and generate a new set of flat trees
+  var stemmedPaths = rootToLeaf(tree).map(function(path) {
+    var stemmed = path.ids.slice(-depth);
     return {
-      pos: path.pos + (path.ids.length - olength),
-      ids: ids
+      pos: path.pos + (path.ids.length - stemmed.length),
+      ids: pathToTree(stemmed)
     };
   });
-
+  // Then we remerge all those flat trees together, ensuring that we dont
+  // connect trees that would go beyond the depth limit
   return stemmedPaths.reduce(function(prev, current, i, arr) {
-    return merge_one(prev, current, true).tree;
+    return doMerge(prev, current, true).tree;
   }, [stemmedPaths.shift()]);
 }
 
+// Merge two trees together
 function mergeTree(tree1, tree2) {
   var conflicts = false;
   for (var i = 0; i < tree2[1].length; i++) {
@@ -71,24 +85,30 @@ function mergeTree(tree1, tree2) {
   return {conflicts: conflicts, tree: tree1};
 }
 
-function merge_one(tree, path, dontExpand) {
+function doMerge(tree, path, dontExpand) {
   var restree = [];
   var conflicts = false;
   var merged = false;
+  var res, branch;
 
   if (!tree.length) {
     return {tree: [path], conflicts: 'new_leaf'};
   }
 
-  for (var i = 0; i < tree.length; i++) {
-    var branch = tree[i];
-    var res;
+  tree.forEach(function(branch) {
     if (branch.pos === path.pos && branch.ids[0] === path.ids[0]) {
+      // Paths start at the same position and have the same root, so they need
+      // merged
       res = mergeTree(branch.ids, path.ids);
       restree.push({pos: branch.pos, ids: res.tree});
       conflicts = conflicts || res.conflicts;
       merged = true;
     } else if (dontExpand !== true) {
+      // The paths start at a different position, take the earliest path and
+      // traverse up until it as at the same point from root as the path we want to
+      // merge if the keys match we return the longer path with the other merged
+      // After stemming we dont want to expand the trees
+
       // destructive assignment plz
       var t1 = branch.pos < path.pos ? branch : path;
       var t2 = branch.pos < path.pos ? path : branch;
@@ -100,20 +120,21 @@ function merge_one(tree, path, dontExpand) {
         tmp = tmp[1][0];
       }
 
-      if (tmp[0] === t2.ids[0]) {
+      if (tmp[0] !== t2.ids[0]) {
+        restree.push(branch);
+      } else {
         res = mergeTree(tmp, t2.ids);
         parent[0] = res.tree;
         restree.push({pos: t1.pos, ids: t1.ids});
         conflicts = conflicts || res.conflicts;
         merged = true;
-      } else {
-        restree.push(branch);
       }
     } else {
       restree.push(branch);
     }
-  }
+  });
 
+  // We didnt find
   if (!merged) {
     restree.push(path);
   }
@@ -128,13 +149,14 @@ function merge_one(tree, path, dontExpand) {
   };
 }
 
-function merge(tree1, path, depth) {
-  tree1 = JSON.parse(JSON.stringify(tree1));
+function merge(tree, path, depth) {
+  // Ugh, nicer way to not modify arguments in place?
+  tree = JSON.parse(JSON.stringify(tree));
   path = JSON.parse(JSON.stringify(path));
-  var tree = merge_one(tree1, path);
+  var newTree = doMerge(tree, path);
   return {
-    tree: stem(tree.tree, depth),
-    conflicts: tree.conflicts
+    tree: stem(newTree.tree, depth),
+    conflicts: newTree.conflicts
   };
 }
 
