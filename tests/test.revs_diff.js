88be62fecd2a10822f5f1eb30c580c9585c84e35
Dale Harvey <dale@arandomurl.com>
2012-03-24 14:02:52 +0000

    Added basic rev diffs test

---
+++ tests/test.revs_diff.js
@@ -0,0 +1,25 @@
+module("revs diff", {
+  setup : function () {
+    this.name = 'test_suite_db';
+  }
+});
+
+asyncTest("Test revs diff", function() {
+  var revs = [];
+  initTestDB(this.name, function(err, db) {
+    db.post({test: "somestuff"}, function (err, info) {
+      revs.push(info.rev);
+      db.put({_id: info.id, _rev: info.rev, another: 'test'}, function(err, info2) {
+        revs.push(info2.rev);
+        db.revsDiff({'somestuff': revs}, function(err, results) {
+          ok(!('somestuff' in results), 'werent missing any revs');
+          revs.push('2-randomid');
+          db.revsDiff({'somestuff': revs}, function(err, results) {
+            ok('somestuff' in results, 'listed missing revs');
+            start();
+          });
+        });
+      });
+    });
+  });
+});
