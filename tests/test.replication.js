9d4e3e23ac8fe13fb1dab9d70e5fed9f79a5922f
Dale Harvey <dale@arandomurl.com>
2012-03-26 21:04:52 +0100

    reformat some of the replication tests

--- tests/test.replication.js
+++ tests/test.replication.js
@@ -1,33 +1,27 @@
-// Just leaving the credentials in there, only ever used to test so worst
-// someone can do is make tests fail
-
-// Couch currently doesnt support CORS, which means replications need to
-// happen from the server the idb database is served from
-var remote = {
-  host: 'localhost:1234'
-};
-
 module('replication', {
   setup : function () {
     this.name = 'test_suite_db';
   }
 });
 
+var remote = {
+  host: 'localhost:1234'
+};
+
+var docs = [
+  {_id: "0", integer: 0, string: '0'},
+  {_id: "1", integer: 1, string: '1'},
+  {_id: "2", integer: 2, string: '2'}
+];
+
 asyncTest("Test basic pull replication", function() {
-
-  var docs = [
-    {_id: "0", integer: 0, string: '0'},
-    {_id: "1", integer: 1, string: '1'},
-    {_id: "2", integer: 2, string: '2'}
-  ];
-
   var remoteUrl = 'http://' + remote.host + '/test_suite_db/';
-
   initTestDB(this.name, function(err, db) {
     initTestDB(remoteUrl, function(err, remote) {
       remote.bulkDocs({docs: docs}, {}, function(err, results) {
         db.replicate.from(remoteUrl, function(err, result) {
-          ok(result.length === docs.length);
+          ok(result.ok, 'replication was ok');
+          ok(result.docs_written = docs.length, 'correct # docs written');
           start();
         });
       });
@@ -36,21 +30,14 @@ asyncTest("Test basic pull replication", function() {
 });
 
 asyncTest("Local DB contains documents", function() {
-  var docs = [
-    {_id: "0", integer: 0, string: '0'},
-    {_id: "1", integer: 1, string: '1'},
-    {_id: "2", integer: 2, string: '2'}
-  ];
-
   var remoteUrl = 'http://' + remote.host + '/test_suite_db/';
-
   initTestDB(this.name, function(err, db) {
     initTestDB(remoteUrl, function(err, remote) {
       remote.bulkDocs({docs: docs}, {}, function(err, _) {
         db.bulkDocs({docs: docs}, {}, function(err, _) {
           db.replicate.from(remoteUrl, function(err, _) {
             db.allDocs(function(err, result) {
-              ok(result.rows.length === docs.length);
+              ok(result.rows.length === docs.length, 'correct # docs exist');
               start();
             });
           });
