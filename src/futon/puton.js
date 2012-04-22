5bc8b454f31b8f37e3d1d93ecd880212f3d90738
gabrielmancini <gabriel.mancini@gmail.com>
2012-04-21 22:58:13 -0300

    add option parameter to path of futon to load the tpl

--- src/futon/puton.js
+++ src/futon/puton.js
@@ -5,7 +5,7 @@
 	var PutonClass = function(el){
 		var self = this;
 		this.$el = $(el);
- 
+		
 		this.init = function() {
 			console.log(self);
 			console.log(self.$el);
@@ -23,8 +23,7 @@
 			window.webkitIndexedDB.getDatabaseNames()
 		);
 	};
-	
-	
+		
 	PutonClass.prototype.tpl = {
 
 	    // Hash of preloaded templates for the app
@@ -39,12 +38,11 @@
 
 	        var loadTemplate = function(index) {
 	            var name = names[index];
-	            console.log('Loading template: ' + name);
+//	            console.log('Loading template: ' + name);
 				
-				//Fix this path retrive by option of plugin
-				var path = '../../src/futon/';
+				var path = PutonClass.prototype.option.pathPuton;
 				
-	            $.get(path + 'tpl/' + name + '.html', function(data) {
+	            $.get(path + '/tpl/' + name + '.html', function(data) {
 	                self.templates[name] = data;
 	                index++;
 	                if (index < names.length) {
@@ -69,7 +67,7 @@
 		var self = this;
 		
 		this.tpl.loadTemplates(['changes_tpl', 'config_section_tpl', 'config_top_tpl', 'confirm_tpl', 'couchapps_tpl', 'create_doc_tpl', 'database_tpl', 'database_view_tpl', 'databases_tpl', 'document_tpl', 'edit_key_tpl', 'home_tpl', 'logged_in', 'logged_in_btn', 'logged_out', 'logged_out_btn', 'replication_doc_tpl', 'replication_items', 'replication_tpl', 'tasks_tpl', 'unauthorized_tpl'], function() {
-		    console.log(self.tpl.templates);
+		  //  console.log(self.tpl.templates);
 		});
 	
 	};
@@ -79,7 +77,10 @@
 		this.$el.html('<div id="dbs">xxx</div>');
 	};
  
-	$.fn.Puton = function(){
+	$.fn.Puton = function(options){
+		
+		PutonClass.prototype.option = options;
+
 		return this.each(function(){
 			(new PutonClass(this));
 		});
