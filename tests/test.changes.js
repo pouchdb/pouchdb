a86c2c4d97f39456f833a3807d81bb0e2a5e7741
Dale Harvey <dale@arandomurl.com>
2012-05-05 16:41:17 +0200

    Fix continuous changes test

--- tests/test.changes.js
+++ tests/test.changes.js
@@ -24,20 +24,15 @@ asyncTest("All changes", function () {
 asyncTest("Continuous changes", function() {
   initTestDB(this.name, function(err, db) {
     var count = 0;
-    db.changes({
+    var changes = db.changes({
       onChange: function(change) { count += 1; },
-      continuous : true,
-      seq : db.seq + 1,
-      complete: function() {
-        equal(count, 0);
-        db.post({test:"another"}, function(err, info) {
-          setTimeout(function() {
-            equal(count, 1);
-            start();
-          }, 50);
-        });
-      }
+      continuous: true
+    });
+    db.post({test:"adoc"}, function(err, info) {
+      setTimeout(function() {
+        equal(count, 1);
+        start();
+      }, 50);
     });
   });
 });
-
