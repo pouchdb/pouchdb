3bd75d9ff2682dfe0d8ce27d6218e2de737703a6
Dale Harvey <dale@arandomurl.com>
2012-05-29 01:32:10 +0100

    Test revs_diff over both idb + http adapters

--- tests/test.revs_diff.js
+++ tests/test.revs_diff.js
@@ -1,26 +1,30 @@
-module("revs diff", {
-  setup : function () {
-    this.name = 'idb://test_suite_db';
-  }
-});
+['idb-1', 'http-1'].map(function(adapter) {
 
-asyncTest("Test revs diff", function() {
-  var revs = [];
-  initTestDB(this.name, function(err, db) {
-    db.post({test: "somestuff", _id: 'somestuff'}, function (err, info) {
-      revs.push(info.rev);
-      db.put({_id: info.id, _rev: info.rev, another: 'test'}, function(err, info2) {
-        revs.push(info2.rev);
-        db.revsDiff({'somestuff': revs}, function(err, results) {
-          ok(!('somestuff' in results), 'werent missing any revs');
-          revs.push('2-randomid');
+  module("revs diff:" + adapter, {
+    setup : function () {
+      this.name = generateAdapterUrl(adapter);
+    }
+  });
+
+  asyncTest("Test revs diff", function() {
+    var revs = [];
+    initTestDB(this.name, function(err, db) {
+      db.post({test: "somestuff", _id: 'somestuff'}, function (err, info) {
+        revs.push(info.rev);
+        db.put({_id: info.id, _rev: info.rev, another: 'test'}, function(err, info2) {
+          revs.push(info2.rev);
           db.revsDiff({'somestuff': revs}, function(err, results) {
-            ok('somestuff' in results, 'listed missing revs');
-            ok(results.somestuff.missing.length === 1, 'listed currect number of');
-            start();
+            ok(!('somestuff' in results), 'werent missing any revs');
+            revs.push('2-randomid');
+            db.revsDiff({'somestuff': revs}, function(err, results) {
+              ok('somestuff' in results, 'listed missing revs');
+              ok(results.somestuff.missing.length === 1, 'listed currect number of');
+              start();
+            });
           });
         });
       });
     });
   });
+
 });
