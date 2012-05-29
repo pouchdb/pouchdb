b5d74fe74ea5d64bc66602b6ea0f180e66441699
Dale Harvey <dale@arandomurl.com>
2012-05-29 02:05:15 +0100

    Test attachments over both idb + http adapters

--- tests/test.attachments.js
+++ tests/test.attachments.js
@@ -1,62 +1,61 @@
-var remote = {
-  host: 'localhost:1234'
-};
+['idb-1', 'http-1'].map(function(adapter) {
 
-module('attachments', {
-  setup : function () {
-    this.name = 'idb://test_suite_db';
-    this.remote = 'http://' + remote.host + '/test_suite_db/';
-    //this.name = this.remote;
-  }
-});
+  module('attachments: ' + adapter, {
+    setup : function () {
+      this.name = generateAdapterUrl(adapter);
+    }
+  });
 
-var binAttDoc = {
-  _id: "bin_doc",
-  _attachments:{
-    "foo.txt": {
-      content_type:"text/plain",
-      data: "VGhpcyBpcyBhIGJhc2U2NCBlbmNvZGVkIHRleHQ="
+  var binAttDoc = {
+    _id: "bin_doc",
+    _attachments:{
+      "foo.txt": {
+        content_type:"text/plain",
+        data: "VGhpcyBpcyBhIGJhc2U2NCBlbmNvZGVkIHRleHQ="
+      }
+    }
+  };
+
+  // empty attachment
+  var binAttDoc2 = {
+    _id: "bin_doc2",
+    _attachments:{
+      "foo.txt": {
+        content_type:"text/plain",
+        data: ""
+      }
     }
   }
-};
 
-// empty attachment
-var binAttDoc2 = {
-  _id: "bin_doc2",
-  _attachments:{
-    "foo.txt": {
-      content_type:"text/plain",
-      data: ""
-    }
-  }
-}
-
-asyncTest("Test some attachments", function() {
-  var db;
-  initTestDB(this.name, function(err, _db) {
-    db = _db;
-    db.put(binAttDoc, function(err, write) {
-      ok(!err, 'saved doc with attachment');
-      db.get('bin_doc/foo.txt', function(err, res) {
-        ok(res === 'This is a base64 encoded text', 'Correct data returned');
-        db.put(binAttDoc2, function(err, rev) {
-          db.get('bin_doc2/foo.txt', function(err, res, xhr) {
-            ok(res === '', 'Correct data returned');
-            moreTests(rev.rev);
-           });
+  asyncTest("Test some attachments", function() {
+    var db;
+    initTestDB(this.name, function(err, _db) {
+      db = _db;
+      db.put(binAttDoc, function(err, write) {
+        ok(!err, 'saved doc with attachment');
+        db.get('bin_doc/foo.txt', function(err, res) {
+          console.log(arguments);
+          ok(res === 'This is a base64 encoded text', 'Correct data returned');
+          db.put(binAttDoc2, function(err, rev) {
+            db.get('bin_doc2/foo.txt', function(err, res, xhr) {
+              ok(res === '', 'Correct data returned');
+              moreTests(rev.rev);
+            });
+          });
         });
       });
     });
-  });
 
-  function moreTests(rev) {
-    var ndoc = 'This is no base64 encoded text';
-    db.putAttachment('bin_doc2/foo2.txt', rev, ndoc, "text/plain", function() {
-      db.get('bin_doc2/foo2.txt', function(err, res, xhr) {
-        ok(res === 'This is no base64 encoded text', 'Correct data returned');
-        start();
+    function moreTests(rev) {
+      var ndoc = 'This is no base64 encoded text';
+      db.putAttachment('bin_doc2/foo2.txt', rev, ndoc, "text/plain", function() {
+        db.get('bin_doc2/foo2.txt', function(err, res, xhr) {
+          ok(res === 'This is no base64 encoded text', 'Correct data returned');
+          start();
+        });
       });
-    });
-  };
+    };
+
+  });
 
 });
