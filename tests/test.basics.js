ecd3a585f5c0c15c340f90e32607ca8141201255
John Chesley <john@chesl.es>
2012-09-20 13:25:19 -0600

    some hacks and things to get things in the right place to run tests

--- tests/test.basics.js
+++ tests/test.basics.js
@@ -1,6 +1,14 @@
-['idb-1', 'http-1'].map(function(adapter) {
+var Pouch = require('../src/pouch.js')
+  , LevelPouch = require('../src/adapters/pouch.leveldb.js')
+  , utils = require('./test.utils.js')
 
-  module("basics: " + adapter, {
+for (var k in utils) {
+  global[k] = global[k] || utils[k];
+}
+
+['ldb-1'].map(function(adapter) {
+
+  QUnit.module("basics: " + adapter, {
     setup : function () {
       this.name = generateAdapterUrl(adapter);
     }
