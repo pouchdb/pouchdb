49711da68e6eb936de60ce21492f3c125eb88c41
John Laxson <john@wheelz.com>
2013-01-11 11:16:59 -0800

    Additional tests for changes api

--- tests/test.changes.js
+++ tests/test.changes.js
@@ -181,4 +181,132 @@ adapters.map(function(adapter) {
     });
   });
 
+  asyncTest("Changes to same doc are grouped", function() {
+    var docs1 = [
+      {_id: "0", integer: 0},
+      {_id: "1", integer: 1},
+      {_id: "2", integer: 2},
+      {_id: "3", integer: 3}
+    ];
+
+    var docs2 = [
+      {_id: "2", integer: 11},
+      {_id: "3", integer: 12}
+    ];
+
+    initTestDB(this.name, function(err, db) {
+      db.bulkDocs({docs: docs1}, function(err, info) {
+        docs2[0]._rev = info[2].rev;
+        docs2[1]._rev = info[3].rev;
+        db.put(docs2[0], function(err, info) {
+          db.put(docs2[1], function(err, info) {
+            db.changes({
+              include_docs: true,
+              complete: function(err, changes) {
+                ok(changes, "got changes");
+                ok(changes.results, "changes has results array");
+                equal(changes.results.length, 4, "should get only 4 changes");
+                equal(changes.results[2].seq, 5, "results have sequence number");
+                equal(changes.results[2].id, "2");
+                equal(changes.results[2].changes.length, 1, "Should include the current revision for a doc");
+                equal(changes.results[2].doc.integer, 11, "Includes correct revision of the doc");
+
+                start();
+              }
+            })
+          })
+        })
+      })
+    })
+  });
+
+  asyncTest("Changes with conflicts are handled correctly", function() {
+    var docs1 = [
+      {_id: "0", integer: 0},
+      {_id: "1", integer: 1},
+      {_id: "2", integer: 2},
+      {_id: "3", integer: 3}
+    ];
+
+    var docs2 = [
+      {_id: "2", integer: 11},
+      {_id: "3", integer: 12}
+    ];
+
+    var localname = this.name, remotename = this.name + "-remote";
+
+    initDBPair(localname, remotename, function(localdb, remotedb) {
+
+      localdb.bulkDocs({docs: docs1}, function(err, info) {
+        docs2[0]._rev = info[2].rev;
+        var rev1 = docs2[1]._rev = info[3].rev;
+        localdb.put(docs2[0], function(err, info) {
+          localdb.put(docs2[1], function(err, info) {
+            var rev2 = info.rev;
+            Pouch.replicate(localdb, remotedb, function(err, done) {
+              // update remote once, local twice, then replicate from remote to local so the remote losing conflict is later in the tree
+              localdb.put({_id: "3", _rev: rev2, integer: 20}, function(err, resp) {
+                var rev3local = resp.rev;
+                localdb.put({_id: "3", _rev: rev3local, integer: 30}, function(err, resp) {
+                  var rev4local = resp.rev
+                  remotedb.put({_id: "3", _rev: rev2, integer: 100}, function(err, resp) {
+                    var remoterev = resp.rev
+                    Pouch.replicate(remotedb, localdb, function(err, done) {
+                      localdb.changes({
+                        include_docs: true,
+                        style: 'all_docs',
+                        complete: function(err, changes) {
+                          ok(changes, "got changes");
+                          ok(changes.results, "changes has results array");
+                          equal(changes.results.length, 4, "should get only 4 changes");
+                          var ch = changes.results[3]
+                          equal(ch.id, "3");
+                          equal(ch.changes.length, 2, "Should include both conflicting revisions");
+                          equal(ch.doc.integer, 30, "Includes correct value of the doc");
+                          equal(ch.doc._rev, rev4local, "Includes correct revision of the doc");
+                          deepEqual(ch.changes, [{rev:rev4local}, {rev:remoterev}], "Includes correct changes array")
+
+                          start();
+                        }
+                      })
+                    })
+                  })
+                })
+              })
+            })
+          })
+        })
+      })
+    })
+  });
+
+  asyncTest("Change entry for a deleted doc", function() {
+    var docs1 = [
+      {_id: "0", integer: 0},
+      {_id: "1", integer: 1},
+      {_id: "2", integer: 2},
+      {_id: "3", integer: 3}
+    ];
+
+    initTestDB(this.name, function(err, db) {
+      db.bulkDocs({docs: docs1}, function(err, info) {
+        var rev = info[3].rev;
+        db.remove({_id: "3", _rev: rev}, function(err, info) {
+          db.changes({
+            include_docs: true,
+            complete: function(err, changes) {
+              ok(changes, 'got Changes');
+              equal(changes.results.length, 4, "should get only 4 changes");
+              var ch = changes.results[3];
+              equal(ch.id, "3", "Have correct doc");
+              equal(ch.seq, 5, "Have correct sequence")
+              equal(ch.deleted, true, "Shows doc as deleted");
+              start();
+            }
+          })
+        })
+      })
+    })
+  });
+
 });
