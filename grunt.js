2ff5ec79bb7b6d2e7bd06a06ef9945cdda77aaf8
Dale Harvey <dale@arandomurl.com>
2013-01-27 08:35:42 +0100

    Remove saucelabs tunnel

--- grunt.js
+++ grunt.js
@@ -97,7 +97,7 @@ module.exports = function(grunt) {
 	tags: [process.env.TRAVIS_BRANCH || "unknown"],
 	testTimeout: 1000 * 60 * 15, // 15 minutes
 	testInterval: 1000 * 30, // 30 seconds
-        tunnelTimeout: 1000 * 60 * 15, // 15 minutes
+        tunneled: false,
         concurrency: 3,
 	urls: ["http://tests.pouchdb.com/tests/test.html?test=release-min&id=" +
                testStartTime.getTime() + "&testFiles=" + testFiles.join(',')],
@@ -155,7 +155,7 @@ module.exports = function(grunt) {
 	  passed: !!(testResults[key].passed),
 	  report: testResults[key]
 	};
-  console.log("Test Result for %s is %s".yellow , key , results.runs[key].passed);
+        console.log("Test Result for %s is %s".yellow , key , results.runs[key].passed);
 	results.passed = results.passed && results.runs[key].passed;
       }
       nano(grunt.config("publish-results.server"))
