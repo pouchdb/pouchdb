ec8835c13d0959cfdbda2f6c7cec4ce79fae8884
Ben Rudolph <rudolphben@gmail.com>
2013-02-19 00:25:26 -0800

    Removed code duplication in idv, leveled and websql adapters.

--- src/pouch.adapter.js
+++ src/pouch.adapter.js
@@ -4,29 +4,142 @@
  * A generic pouch adapter
  */
 var PouchAdapter = function(opts, callback) {
-  var adapter = {};
+  var api = {};
 
-  var adapter = Pouch.adapters[opts.adapter](opts, callback);
+  var api = Pouch.adapters[opts.adapter](opts, callback);
 
-  adapter.replicate = {};
+  api.replicate = {};
 
-  adapter.replicate.from = function (url, opts, callback) {
+  if (!api.post) {
+    api.post = function (doc, opts, callback) {
+      if (typeof opts === 'function') {
+        callback = opts;
+        opts = {};
+      }
+      return api.bulkDocs({docs: [doc]}, opts, yankError(callback));
+    }
+  }
+
+  if (!api.put) {
+
+    api.put = function(doc, opts, callback) {
+      if (typeof opts === 'function') {
+        callback = opts;
+        opts = {};
+      }
+
+      if (!doc || !('_id' in doc)) {
+        return call(callback, Pouch.Errors.MISSING_ID);
+      }
+      return api.bulkDocs({docs: [doc]}, opts, yankError(callback));
+    }
+
+  }
+
+  if (!api.putAttachment) {
+    api.putAttachment = api.putAttachment = function (id, rev, doc, type, callback) {
+      id = parseDocId(id);
+      api.get(id.docId, {attachments: true}, function(err, obj) {
+        obj._attachments || (obj._attachments = {});
+        obj._attachments[id.attachmentId] = {
+          content_type: type,
+          data: btoa(doc)
+        }
+        api.put(obj, callback);
+      });
+    };
+  }
+
+  if (!api.removeAttachment) {
+     api.removeAttachment = function idb_removeAttachment(id, rev, callback) {
+      id = parseDocId(id);
+      api.get(id.docId, function(err, obj) {
+        if (err) {
+          call(callback, err);
+          return;
+        }
+
+        if (obj._rev != rev) {
+          call(callback, Pouch.Errors.REV_CONFLICT);
+          return;
+        }
+
+        obj._attachments || (obj._attachments = {});
+        delete obj._attachments[id.attachmentId];
+        api.put(obj, callback);
+      });
+    };
+  }
+
+  if (!api.remove) {
+    api.remove = function (doc, opts, callback) {
+      if (typeof opts === 'function') {
+        callback = opts;
+        opts = {};
+      }
+      if (opts === undefined) {
+        opts = {};
+      }
+      opts.was_delete = true;
+      var newDoc = extend(true, {}, doc);
+      newDoc._deleted = true;
+      return api.bulkDocs({docs: [newDoc]}, opts, yankError(callback));
+    };
+
+  }
+
+  if (!api.revsDiff) {
+    api.revsDiff = function idb_revsDiff(req, opts, callback) {
+      if (typeof opts === 'function') {
+        callback = opts;
+        opts = {};
+      }
+      var ids = Object.keys(req);
+      var count = 0;
+      var missing = {};
+
+      function readDoc(err, doc, id) {
+        req[id].map(function(revId) {
+          var matches = function(x) { return x.rev !== revId; };
+          if (!doc || doc._revs_info.every(matches)) {
+            if (!missing[id]) {
+              missing[id] = {missing: []};
+            }
+            missing[id].missing.push(revId);
+          }
+        });
+
+        if (++count === ids.length) {
+          return call(callback, null, missing);
+        }
+      }
+
+      ids.map(function(id) {
+        api.get(id, {revs_info: true}, function(err, doc) {
+          readDoc(err, doc, id);
+        });
+      });
+    };
+
+  }
+
+  api.replicate.from = function (url, opts, callback) {
     if (typeof opts === 'function') {
       callback = opts;
       opts = {};
     }
-    return Pouch.replicate(url, adapter, opts, callback);
+    return Pouch.replicate(url, api, opts, callback);
   };
 
-  adapter.replicate.to = function (dbName, opts, callback) {
+  api.replicate.to = function (dbName, opts, callback) {
     if (typeof opts === 'function') {
       callback = opts;
       opts = {};
     }
-    return Pouch.replicate(adapter, dbName, opts, callback);
+    return Pouch.replicate(api, dbName, opts, callback);
   };
 
-  return adapter;
+  return api;
 };
 
 if (typeof module !== 'undefined' && module.exports) {
