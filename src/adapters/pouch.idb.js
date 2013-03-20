5ae54af42a84864ac23fe16343a4fcec7f02cc9c
Howon Lee <hlee.howon@gmail.com>
2013-03-20 00:50:47 -0700

    Revert "add chrome.storage.local functionality"
    This reverts commit 6ff8fea64daf612ad5e9a08e6be4943459357c5d.

--- src/adapters/pouch.idb.js
+++ src/adapters/pouch.idb.js
@@ -230,19 +230,7 @@ var IdbPouch = function(opts, callback) {
         }
 
         IdbPouch.Changes.notify(name);
-        if (typeof chrome !== "undefined" && typeof chrome.storage !== "undefined" && typeof chrome.storage.local !== "undefined"){
-          var cb = function(items){
-            if (items[name]){
-              return items[name] === "a";
-            }
-            return false;
-          };
-          var ab = chrome.storage.local.get(name, cb) ? "b" : "a";
-          chrome.storage.local.set({name: ab});
-        }
-        else if (localStorage){
-          localStorage[name] = (localStorage[name] === "a") ? "b" : "a";
-        }
+        localStorage[name] = (localStorage[name] === "a") ? "b" : "a";
       });
       call(callback, null, aresults);
     }
