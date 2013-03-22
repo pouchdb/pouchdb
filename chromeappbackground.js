0468ed7f1615ec75bc601a38dbd01a04229268e2
Howon Lee <hlee.howon@gmail.com>
2013-03-21 18:16:56 -0700

    app testing reimplemented

--- chromeappbackground.js
+++ chromeappbackground.js
@@ -1,6 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function(){
-	chrome.app.window.create("./tests/test.html", {
-		"width": 800,
+	chrome.app.window.create("./tests/testWrapper.html", {
+		"width": 1000,
 		"height": 800
 	});
 });
