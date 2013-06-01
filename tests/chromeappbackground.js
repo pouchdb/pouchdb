f4afa2c061ee2875c2d3ff4877ce95857ef447b7
Dale Harvey <dale@arandomurl.com>
2013-05-31 18:07:05 -0700

    (#614) - Follow up to fix linting

--- tests/chromeappbackground.js
+++ tests/chromeappbackground.js
@@ -1,6 +1,10 @@
+/*globals chrome */
+
+'use strict';
+
 chrome.app.runtime.onLaunched.addListener(function(){
-	chrome.app.window.create("./test.html", {
-		"width": 1000,
-		"height": 800
-	});
+  chrome.app.window.create("./test.html", {
+    "width": 1000,
+    "height": 800
+  });
 });
