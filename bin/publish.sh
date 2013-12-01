7d5a67950f0864e32991665cf641ed7d268bca68
Dale Harvey <dale@arandomurl.com>
2013-12-01 10:57:30 +0000

    (#1062) - Add publish-site script to upload site + nightly

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 # publish requires jam and tin installed
 # install with npm install tin jamjs
+
+# Build
 git checkout -b build
 ./node_modules/tin/bin/tin -v $1
 echo "module.exports = '"$1"';" > src/version.js
 npm run build
 git add dist -f
-git add src/version.js
-git add package.json
-git add bower.json
-git add component.json
+git add src/version.js package.json bower.json component.json
 git commit -m "build $1"
+
+# Tag and push
 git tag $1
 git push --tags git@github.com:daleharvey/pouchdb.git $1
+
+# Publish JS modules
 npm publish
 ./node_modules/jamjs/bin/jam.js publish
+
+# Build pouchdb.com
+cd docs
+jekyll build
+cd ..
+
+# Publish pouchdb.com + nightly
+scp -r docs/_site/* pouchdb.com:www/pouchdb.com
+scp dist/* pouchdb.com:www/download.pouchdb.com
+
+# Cleanup
 git checkout master
 git branch -D build
