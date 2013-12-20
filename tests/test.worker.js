8f73143fa9d2f019871ba00cfc532f90b20625ec
Calvin Metcalf <cmetcalf@appgeo.com>
2013-12-20 15:15:28 -0500

    (#1166) - web workers

---
+++ tests/test.worker.js
@@ -0,0 +1,32 @@
+QUnit.module('worker');
+
+asyncTest('create it',1,function(){
+  var worker = new Worker('worker.js');
+  worker.addEventListener('message',function(e){
+    ok('pong',e.data);
+    worker.terminate();
+    start();
+  });
+  worker.postMessage('ping');
+});
+asyncTest('check pouch version',1,function(){
+  var worker = new Worker('worker.js');
+  worker.addEventListener('message',function(e){
+    ok(PouchDB.version,e.data);
+    worker.terminate();
+    start();
+  });
+  worker.postMessage('version');
+});
+asyncTest('create db',1,function(){
+  var worker = new Worker('worker.js');
+  worker.addEventListener('error',function(e){
+    throw e;
+  });
+  worker.addEventListener('message',function(e){
+    ok('lala',e.data);
+    worker.terminate();
+    start();
+  });
+  worker.postMessage(['create',testUtils.generateAdapterUrl(adapter)]);
+});
