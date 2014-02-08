0eec8e78324de65095e7a585e35856edad5c0a2b
Maciej Dębski <md319428@students.mimuw.edu.pl>
2014-02-08 09:41:10 -0800

    (#1147) - run watchify from dev-server.js

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -5,6 +5,38 @@
 var cors_proxy = require("corsproxy");
 var http_proxy = require("http-proxy");
 var http_server = require("http-server");
+var fs = require('fs');
+var through = require('through');
+
+fs.mkdir('dist', function(e) {
+  if(e && e.code != 'EEXIST') {
+    throw e;
+  }
+});
+
+var watchify = require("watchify");
+var w = watchify("./lib/index.js");
+var dotfile = "./dist/.pouchdb-nightly.js";
+var outfile = "./dist/pouchdb-nightly.js";
+
+w.on('update', bundle);
+bundle();
+
+function bundle () {
+  var wb = w.bundle();
+  wb.on('error', function (err) {
+    console.error(String(err));
+  });
+  wb.pipe(fs.createWriteStream(dotfile));
+  wb.pipe(through(function write() {}, end));
+
+  function end () {
+    fs.rename(dotfile, outfile, function (err) {
+      if(err) return console.error(err);
+      console.log("Updated " + outfile);
+    });
+  }
+}
 
 var program = require('commander');
 
