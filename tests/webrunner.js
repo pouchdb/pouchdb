0655b6c0d5327d432d7719ed70e6f4fda5c118f7
Calvin Metcalf <cmetcalf@appgeo.com>
2014-02-13 17:04:33 -0500

    (#1393) - Errors in browser tests

--- tests/webrunner.js
+++ tests/webrunner.js
@@ -2,14 +2,20 @@
 'use strict';
 var runner = mocha.run();
 var results = {};
+
 results.passed = 0;
 results.failed = 0;
 results.total = 0;
+
+results.failures = [];
+
 runner.on('pass', function () {
   results.passed++;
   results.total++;
 });
-runner.on('fail', function () {
+
+runner.on('fail', function(e) {
+  results.failures.push(e);
   results.failed++;
   results.total++;
 });
