d9c3cf942cd301d97861bf297656e06a445a1504
Dale Harvey <dale@arandomurl.com>
2014-02-21 09:51:02 +0000

    Revert "(#1449) - move uuid files into folders (backwards compatibly) also smooth out creation process to make it async and adds db.destroy"
    This reverts commit bee0eb8015f38a684c063d16180533a72287ca56.

--- tests/test.issue915.js
+++ tests/test.issue915.js
@@ -3,22 +3,22 @@ var fs = require('fs');
 describe('Remove DB', function () {
   beforeEach(function (done) {
     //Create a dir
-    fs.mkdir('tmp/veryimportantfiles', function () {
+    fs.mkdir('veryimportantfiles', function () {
       done();
     });
   });
   afterEach(function (done) {
     PouchDB.destroy('name', function () {
-      fs.rmdir('tmp/veryimportantfiles', function () {
+      fs.rmdir('veryimportantfiles', function () {
         done();
       });
     });
   });
   it('Create a pouch without DB setup', function (done) {
     new PouchDB('name', { skipSetup: true }, function () {
-      PouchDB.destroy('tmp/veryimportantfiles', function (error, response) {
+      PouchDB.destroy('veryimportantfiles', function (error, response) {
         error.message.should.equal('Database not found', 'should return Database not found error');
-        fs.exists('tmp/veryimportantfiles', function (resp) {
+        fs.exists('veryimportantfiles', function (resp) {
           resp.should.equal(true, 'veryimportantfiles was not removed');
           done();
         });
