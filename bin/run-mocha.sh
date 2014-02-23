003c26722197806132368e5c417ce949cf533c31
Calvin Metcalf <cmetcalf@appgeo.com>
2014-02-22 20:51:57 -0500

    (#1474) - fix coverage

--- bin/run-mocha.sh
+++ bin/run-mocha.sh
@@ -14,8 +14,8 @@ if [ ! $COVERAGE ]; then
 else
     ./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha -- \
         --bail \
-        --timeout=$TIMEOUT \
-        -require=./tests/node.setup.js \
+        --timeout $TIMEOUT \
+        --require=./tests/node.setup.js \
         --reporter=$REPORTER \
         --grep=$GREP \
         tests/test.*.js
