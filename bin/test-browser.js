9171a87ad9e97c519032068f48b10c9ac6a91fe9
Dale Harvey <dale@arandomurl.com>
2014-02-25 04:30:25 +0000

    (#1343) - Enable running tests against saucelabs on travis

--- bin/test-browser.js
+++ bin/test-browser.js
@@ -1,58 +1,40 @@
 #!/usr/bin/env node
-"use strict";
+'use strict';
 
 var path = require('path');
 var spawn = require('child_process').spawn;
 
 var request = require('request');
 var wd = require('wd');
+var sauceConnectLauncher = require('sauce-connect-launcher');
+
 var devserver = require('./dev-server.js');
 
 var SELENIUM_PATH = '../vendor/selenium-server-standalone-2.38.0.jar';
+var SELENIUM_HUB = 'http://localhost:4444/wd/hub/status';
 var testUrl = 'http://127.0.0.1:8000/tests/test.html';
 var testTimeout = 30 * 60 * 1000;
 
+var username = process.env.SAUCE_USERNAME;
+var accessKey = process.env.SAUCE_ACCESS_KEY;
+var browser = process.env.CLIENT || 'firefox';
 var client;
 
 if (process.env.GREP) {
   testUrl += '?grep=' + process.env.GREP;
 }
 
-function startServers(callback) {
-
-  // Starts the file and CORS proxy
-  devserver.start();
-
-  // Start selenium
-  spawn('java', ['-jar', path.resolve(__dirname, SELENIUM_PATH)], {});
-
-  var retries = 0;
-  var started = function () {
-
-    if (++retries > 30) {
-      console.error('Unable to connect to selenium');
-      process.exit(1);
-      return;
-    }
-
-    request('http://localhost:4444/wd/hub/status', function (err, resp) {
-      if (resp && resp.statusCode === 200) {
-        callback();
-      } else {
-        setTimeout(started, 1000);
-      }
-    });
-  };
-
-  started();
+if ((process.env.TRAVIS && browser !== 'firefox') &&
+    !process.env.TRAVIS_SECURE_ENV_VARS) {
+  console.error('Not running test, cannot connect to saucelabs');
+  process.exit(0);
 }
 
 function testError(e) {
   console.error(e);
   console.error('Doh, tests failed');
-  client.quit().then(function () {
-    process.exit(3);
-  });
+  client.quit();
+  process.exit(3);
 }
 
 function testComplete(result) {
@@ -72,25 +54,82 @@ function testComplete(result) {
 
 }
 
-function startTest() {
+function startSelenium(callback) {
 
-  var browser = process.env.CLIENT || 'firefox';
-  console.log('Starting', browser);
+  // Start selenium
+  spawn('java', ['-jar', path.resolve(__dirname, SELENIUM_PATH)], {});
 
-  var script =
-    'var cb = arguments[arguments.length - 1];' +
-    'runner.on("end", function() { cb(results); });' +
-    'runner.on("fail", function() { cb(results); });';
+  var retries = 0;
+  var started = function () {
 
-  client = wd.promiseChainRemote();
-  client
-    .init({browserName: browser})
-    .get(testUrl)
-    .setAsyncScriptTimeout(testTimeout)
-    .executeAsync(script)
-    .then(testComplete, testError);
+    if (++retries > 30) {
+      console.error('Unable to connect to selenium');
+      process.exit(1);
+      return;
+    }
+
+    request(SELENIUM_HUB, function (err, resp) {
+      if (resp && resp.statusCode === 200) {
+        client = wd.promiseChainRemote();
+        callback();
+      } else {
+        setTimeout(started, 1000);
+      }
+    });
+  };
+
+  started();
+
 }
 
-startServers(function () {
-  startTest();
-});
+function startSauceConnect(callback) {
+
+  var options = {
+    username: username,
+    accessKey: accessKey
+  };
+
+  sauceConnectLauncher(options, function (err, sauceConnectProcess) {
+    client = wd.promiseChainRemote("localhost", 4445, username, accessKey);
+    callback();
+  });
+}
+
+function startTest() {
+
+  console.log('Starting', browser);
+
+  var opts = {
+    browserName: browser,
+    tunnelTimeout: testTimeout,
+    'max-duration': 60 * 30,
+    'command-timeout': 599,
+    'idle-timeout': 599
+  };
+
+  client.init(opts).get(testUrl, function () {
+
+    /* jshint evil: true */
+    var interval = setInterval(function () {
+      client.eval('window.results', function (err, results) {
+        if (err) {
+          clearInterval(interval);
+          testError(err);
+        } else if (results.completed) {
+          clearInterval(interval);
+          testComplete(results);
+        } else {
+          console.log('=> ', results);
+        }
+      });
+    }, 10 * 1000);
+  });
+}
+
+devserver.start();
+
+if (process.env.TRAVIS && browser !== 'firefox') {
+  startSauceConnect(startTest);
+} else {
+  startSelenium(startTest);
+}
