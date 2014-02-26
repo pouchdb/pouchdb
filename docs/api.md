1e2601fbfefa0b2ebe8dba431983e1bcfb8aa702
Calvin Metcalf <cmetcalf@appgeo.com>
2014-02-26 10:32:38 -0500

    (#1115) - Add event emitter for DB change events

--- docs/api.md
+++ docs/api.md
@@ -19,7 +19,7 @@ Additionally, any method that only returns a single thing (e.g. `db.get`, but no
 new PouchDB([name], [options])
 {% endhighlight %}
 
-This method creates a database or opens an existing one. If you use a URL like `http://domain.com/dbname` then PouchDB will work as a client to an online CouchDB instance.  Otherwise it will create a local database using whatever backend is present (i.e. IndexedDB, WebSQL, or LevelDB).
+This method creates a database or opens an existing one. If you use a URL like `http://domain.com/dbname` then PouchDB will work as a client to an online CouchDB instance.  Otherwise it will create a local database using whatever backend is present (i.e. IndexedDB, WebSQL, or LevelDB). 
 
 ### Options
 
@@ -743,6 +743,19 @@ db.revsDiff({
 }
 {% endhighlight %}
 
+## Events<a id="events"></a>
+
+PouchDB is an [event emiter](http://nodejs.org/api/events.html#events_class_events_eventemitter) and will emit a 'created' event when a database is created. A 'destroy' event is emited when a database is destroyed.
+
+{% highlight js %}
+PouchDB.on('created', function (dbName) {
+  // called whenver a db is created.
+});
+PouchDB.on('destroyed', function (dbName) {
+  // called whenver a db is destroyed.
+});
+{% endhighlight %}
+
 ## Plugins<a id="plugins"></a>
 
 Writing a plugin is easy the api is
@@ -753,4 +766,5 @@ PouchDB.plugin({
 });
 {% endhighlight %}
 
-This will add the function as a method of all databases with the given name, it will always be called in the so that `this` is db.
+This will add the function as a method of all databases with the given name, it will always be called in context so that `this` is db.
+
---
+++ tests/test.events.js
@@ -0,0 +1,118 @@
+'use strict';
+
+var adapters = [
+  'http-1',
+  'local-1'
+];
+adapters.forEach(function (adapter) {
+  describe('test.events.js-' + adapter, function () {
+    //we can't use the same db becasue
+    var i = 0;
+    var dbs = {};
+    beforeEach(function () {
+      dbs.name = testUtils.adapterUrl(adapter, 'events_tests' + i++);
+    });
+
+    afterEach(function (done) {
+      PouchDB.destroy(dbs.name, done);
+    });
+
+    it('PouchDB emits creation event', function (done) {
+      PouchDB.once('created', function (name) {
+        name.should.equal(dbs.name, 'should be same thing');
+        done();
+      });
+      new PouchDB(dbs.name);
+    });
+    it('PouchDB emits destruction event', function (done) {
+      new PouchDB(dbs.name + 1, function () {
+        PouchDB.destroy(dbs.name + 1);
+      }).once('destroyed', function () {
+        new PouchDB(dbs.name, function () {
+          done();
+        });
+      });
+    });
+    it('emit creation event', function (done) {
+      var db = new PouchDB(dbs.name).on('created', function (newDB) {
+        db.should.equal(newDB, 'should be same thing');
+        done();
+      });
+    });
+
+    it('emit changes event', function (done) {
+      new PouchDB(dbs.name, function (err, db) {
+        var id = 'emiting';
+        var obj = {
+          something: 'here',
+          somethingElse: 'overHere'
+        };
+        db.on('change', function (change) {
+          change.seq.should.equal(1, 'changed');
+          change.id.should.equal('emiting');
+          done(err);
+        });
+        db.put(obj, id);
+      });
+    });
+
+    it('emit create event', function (done) {
+      new PouchDB(dbs.name, function (err, db) {
+        var id = 'creating';
+        var obj = {
+          something: 'here',
+          somethingElse: 'overHere'
+        };
+        db.on('create', function (change) {
+          change.id.should.equal('creating');
+          change.seq.should.equal(1, 'created');
+          done(err);
+        });
+        db.put(obj, id);
+      });
+    });
+
+    it('emit update event', function (done) {
+      new PouchDB(dbs.name, function (err, db) {
+        var id = 'updating';
+        var obj = {
+          something: 'here',
+          somethingElse: 'overHere'
+        };
+        db.on('update', function (change) {
+          change.id.should.equal('updating');
+          change.seq.should.equal(2, 'seq 2, updated');
+          done(err);
+        });
+
+        db.put(obj, id).then(function (doc) {
+          db.put({'something': 'else'}, id, doc.rev);
+        });
+       
+      });
+    });
+
+    it('emit delete event', function (done) {
+      new PouchDB(dbs.name, function (err, db) {
+        var id = 'emiting';
+        var obj = {
+          something: 'here',
+          somethingElse: 'overHere'
+        };
+        db.on('delete', function (change) {
+          change.seq.should.equal(2, 'deleted');
+          change.id.should.equal('emiting');
+          done(err);
+        });
+
+        db.put(obj, id).then(function (doc) {
+          db.remove({
+            _id: id,
+            _rev: doc.rev
+          });
+        });
+      });
+    });
+
+  });
+});
