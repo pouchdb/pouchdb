6058d9dcdf320a4336ce53fa0571977e76d43d21
Dale Harvey <dale@arandomurl.com>
2014-03-08 12:28:13 +0000

    (#1524) - Add warning when callbacks called more than once

---
+++ tests/unit/test.once.js
@@ -0,0 +1,26 @@
+
+'use strict';
+
+var should = require('chai').should();
+var utils = require('../lib/utils.js');
+
+describe('test.once.js', function () {
+
+  it.skip('Only call once ... once', function () {
+    var myFun = utils.once(function () { });
+    myFun();
+    should.throw(myFun);
+  });
+
+  it.skip('Once wrapped in a promise', function (done) {
+    var callback = function () {};
+    var myFun = utils.toPromise(function (callback) {
+      setTimeout(function () {
+        callback();
+        should.throw(callback);
+        done();
+      });
+    });
+    myFun(callback);
+  });
+});
