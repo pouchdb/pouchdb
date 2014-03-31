e324916e7f1b8f3033cda9ee3ffbd0b9ada1d4a4
Nick Colley <nickcolley7@gmail.com>
2014-03-31 22:26:04 +0100

    (#1765) - Watch Less

--- bin/build-site.sh
+++ bin/build-site.sh
@@ -10,11 +10,7 @@ fi
 # Install npm dependancies
 npm install
 
-# Make the directory for the css to live in.
-mkdir -p docs/static/css
-
-# Pre-process the less into it's final form.
-node_modules/less/bin/lessc docs/static/less/pouchdb/pouchdb.less > docs/static/css/pouchdb.css
+npm run build-css
 
 # Build the site using jekyll
 cd docs
