ce17b02faaece1b889b2bc5e4eebb84101b02315
Erik Waingarten <erikwaing@gmail.com>
2014-04-27 14:21:17 -0400

    (#2054) - PouchDB can save default options that are appended to options on database creation

--- tests/test.constructor.js
+++ tests/test.constructor.js
@@ -70,3 +70,44 @@ describe('constructor errors', function () {
     });
   });
 });
+
+describe('default options', function () {
+
+  it('default objects should be empty by default', function () {
+    PouchDB.defaults().should.deep.equal({});
+  });
+
+  it('default should be able to be set', function () {
+    var opts = { 'foo' : 'foo' };
+    PouchDB.defaults(opts);
+    PouchDB.defaults().should.deep.equal(opts);
+    PouchDB.defaults({});
+  });
+
+  it('default options should be used when creating db', function (done) {
+    var opts = { adapter : 'myFakeAdapter' };
+    PouchDB.defaults(opts);
+    new PouchDB('foo', function (err, db) {
+      should.exist(err);
+      err.message.should
+        .equal('Adapter is missing',
+               'should give the correct error message');
+      done(db);
+    });
+    PouchDB.defaults({});
+  });
+
+  it('default options should be added to the options', function (done) {
+    var opts = { adapter : 'myFakeAdapter' };
+    PouchDB.defaults(opts);
+    new PouchDB({ name: 'foo' }, function (err, db) {
+      should.exist(err);
+      err.message.should
+        .equal('Adapter is missing',
+               'should give the correct error message');
+      done(db);
+    });
+    PouchDB.defaults({});
+  });
+
+});
