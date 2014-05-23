29d01fb40cf76bae0da5532162c01f324a7ae925
Nolan Lawson <nolan.lawson@gmail.com>
2014-05-23 16:36:50 -0700

    (#2190) - remove LEVEL_BACKEND, build adapter plugins

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -4,90 +4,103 @@
 
 var fs = require('fs');
 var glob = require('glob');
+var Promise = require('bluebird');
 var watchGlob = require('watch-glob');
 var watchify = require('watchify');
 var browserify = require('browserify');
-
 var cors_proxy = require('corsproxy');
 var http_proxy = require('http-proxy');
 var http_server = require('http-server');
 
-var performanceBundle = './dist/performance-bundle.js';
-var level_backend = process.env.LEVEL_BACKEND;
-var indexfile, outfile;
 var queryParams = {};
 
 if (process.env.ES5_SHIM || process.env.ES5_SHIMS) {
   queryParams.es5shim = true;
 }
-
-var perfRoot;
-if (process.env.LEVEL_BACKEND) {
-  queryParams.sourceFile = "pouchdb-" + level_backend + ".js";
-  indexfile = "./alt/index-alt.js";
-  outfile = "./dist/pouchdb-" + level_backend + ".js";
-  perfRoot = './alt/performance/*.js';
-} else {
-  indexfile = "./lib/index.js";
-  outfile = "./dist/pouchdb-nightly.js";
-  perfRoot = './tests/performance/*.js';
+if (process.env.ADAPTERS) {
+  queryParams.adapters = process.env.ADAPTERS;
 }
 
-var w = watchify(indexfile).on('update', bundle);
-bundle();
+var indexfile = "./lib/index.js";
+var outfile = "./dist/pouchdb-nightly.js";
+var perfRoot = './tests/performance/*.js';
+var performanceBundle = './dist/performance-bundle.js';
 
-function writeFile(file) {
+var w = watchify(indexfile).on('update', bundle);
+
+function writeFile(file, callback) {
   return function (err, data) {
     if (err) {
       console.log(err);
     } else {
       fs.writeFileSync(file, data);
       console.log('Updated: ', file);
+      if (callback) {
+        callback();
+      }
     }
   };
 }
 
-function bundle() {
-  if (level_backend) {
-    w.require(level_backend, {expose: 'levelalt'});
-    w.require('./alt/index-alt', {expose: './index'});
-  }
-  w.bundle({standalone: "PouchDB"}, writeFile(outfile));
+function bundle(callback) {
+  w.bundle({standalone: "PouchDB"}, writeFile(outfile, callback));
 }
 
-function bundlePerfTests() {
+function bundlePerfTests(callback) {
   glob(perfRoot, function (err, files) {
     var b = browserify(files);
-    if (level_backend) {
-      b.require(level_backend, {expose: 'levelalt'});
-      b.require('./alt/index-alt', {expose: './index'});
-    }
-    b.bundle({}, writeFile(performanceBundle));
+    b.bundle({}, writeFile(performanceBundle, callback));
   });
 }
 
 watchGlob(perfRoot, bundlePerfTests);
-bundlePerfTests();
+
+var filesWritten = false;
+Promise.all([
+  new Promise(function (resolve) {
+    bundle(resolve);
+  }),
+  new Promise(function (resolve) {
+    bundlePerfTests(resolve);
+  })
+]).then(function () {
+  filesWritten = true;
+  checkReady();
+});
 
 var COUCH_HOST = process.env.COUCH_HOST || 'http://127.0.0.1:5984';
 
 var HTTP_PORT = 8000;
 var CORS_PORT = 2020;
 
-function startServers(couchHost) {
-  http_server.createServer().listen(HTTP_PORT);
-  cors_proxy.options = {target: couchHost || COUCH_HOST};
-  http_proxy.createServer(cors_proxy).listen(CORS_PORT);
-  var testRoot = 'http://127.0.0.1:' + HTTP_PORT;
-  var query = '';
-  Object.keys(queryParams).forEach(function (key) {
-    query += (query ? '&' : '?');
-    query += key + '=' + encodeURIComponent(queryParams[key]);
+var serversStarted;
+var readyCallback;
+
+function startServers(callback) {
+  readyCallback = callback;
+  http_server.createServer().listen(HTTP_PORT, function () {
+    cors_proxy.options = {target: COUCH_HOST};
+    http_proxy.createServer(cors_proxy).listen(CORS_PORT, function () {
+      var testRoot = 'http://127.0.0.1:' + HTTP_PORT;
+      var query = '';
+      Object.keys(queryParams).forEach(function (key) {
+        query += (query ? '&' : '?');
+        query += key + '=' + encodeURIComponent(queryParams[key]);
+      });
+      console.log('Integration tests: ' + testRoot +
+        '/tests/test.html' + query);
+      console.log('Performance tests: ' + testRoot +
+        '/tests/performance/test.html');
+      serversStarted = true;
+      checkReady();
+    });
   });
-  console.log('Integration tests: ' + testRoot +
-              '/tests/test.html' + query);
-  console.log('Performance tests: ' + testRoot +
-              '/tests/performance/test.html');
+}
+
+function checkReady() {
+  if (filesWritten && serversStarted && readyCallback) {
+    readyCallback();
+  }
 }
 
 
