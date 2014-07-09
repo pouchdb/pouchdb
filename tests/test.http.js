cdb14de51c3680817045fb5ae7da3e2a483efac3
Derek Ekins <derek@spathi.com>
2014-07-09 09:57:01 +0100

    Added a beforeSend hook to ajax settings. This confirms to the "standard" set by jQuery/zepto when performing ajax requests.
    usage:
    
    new PouchDB(dbs.name, { ajax: {beforeSend: function(xhr, options){ } } });
    
    reason for change:
    
    To manipulate the request when using alternative authentication methods such as OAuth
    
    example:
    
    var OAuth = require('oauth-1.0a')
    
    var oauth = OAuth({
      consumer: {
        public: 'consumerKey1'
        secret: 'key1Secret'
      }
    });
    
    var token = {
      public: 'token1'
      secret: 'token1Secret'
    };
    
    new PouchDB(dbs.name, { ajax: {beforeSend: function(xhr, options){
      authHeaders = oauth.toHeader(oauth.authorize(options, token));
      xhr.setRequestHeader('Authorization', authHeaders.Authorization);
    } } });

--- tests/test.http.js
+++ tests/test.http.js
@@ -57,4 +57,25 @@ describe('test.http.js', function () {
     });
   });
 
+  it('Create a pouch with a beforeSend hook', function (done) {
+    var instantDB;
+    PouchDB.destroy(dbs.name, function () {
+      var beforeSendArguments = null;
+      var ajax = {
+        beforeSend: function(){
+          beforeSendArguments = arguments;
+        }
+      };
+      instantDB = new PouchDB(dbs.name, { ajax: ajax });
+      instantDB.post({ test: 'abc' }, function (err, info) {
+        beforeSendArguments.length.should.equal(2)
+        var xhr = beforeSendArguments[0];
+        var options = beforeSendArguments[1]
+        
+        xhr.should.be.an.instanceof(XMLHttpRequest);
+        options.beforeSend.should.equal(ajax.beforeSend)
+        done();
+      });
+    });
+  });
 });
