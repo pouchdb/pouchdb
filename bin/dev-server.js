b96d213cc220fc3c51a48f60bb7f954bb7f7b58d
Nolan Lawson <nolan.lawson@gmail.com>
2014-07-18 14:09:35 -0700

    (#2035) - make the CORS proxy optional
    The CORS proxy is really flakey, and it contributes to
    our intermittent errors.  However, for reasons that
    are mysterious to me, Saucelabs gives us an error
    without it, on iPhone, Safari, and IE.
    
    This adds an optional flag, CORS_PROXY=true, which
    can be used to enable the proxy.  Otherwise we
    set the CORS config on CouchDB and use 5984 directly.

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -11,6 +11,7 @@ var browserify = require('browserify');
 var cors_proxy = require('corsproxy');
 var http_proxy = require('http-proxy');
 var http_server = require('http-server');
+var request = require('request');
 
 var queryParams = {};
 
@@ -19,6 +20,9 @@ if (process.env.ES5_SHIM || process.env.ES5_SHIMS) {
 }
 if (process.env.ADAPTERS) {
   queryParams.adapters = process.env.ADAPTERS;
+}
+if (process.env.CORS_PROXY) {
+  queryParams.corsProxy = process.env.CORS_PROXY;
 }
 
 var indexfile = "./lib/index.js";
@@ -76,25 +80,77 @@ var CORS_PORT = 2020;
 var serversStarted;
 var readyCallback;
 
-function startServers(callback) {
-  readyCallback = callback;
+function printServerInfo() {
+  var testRoot = 'http://127.0.0.1:' + HTTP_PORT;
+  var query = '';
+  Object.keys(queryParams).forEach(function (key) {
+    query += (query ? '&' : '?');
+    query += key + '=' + encodeURIComponent(queryParams[key]);
+  });
+  console.log('Integration tests: ' + testRoot +
+    '/tests/test.html' + query);
+  console.log('Performance tests: ' + testRoot +
+    '/tests/performance/test.html');
+}
+
+function setUpCors() {
+  // enable CORS globally, because it's easier this way
+
+  var corsValues = {
+    '/_config/httpd/enable_cors': 'true',
+    '/_config/cors/origins': '*',
+    '/_config/cors/credentials': 'true',
+    '/_config/cors/methods': 'PROPFIND, PROPPATCH, COPY, MOVE, DELETE, ' +
+      'MKCOL, LOCK, UNLOCK, PUT, GETLIB, VERSION-CONTROL, CHECKIN, ' +
+      'CHECKOUT, UNCHECKOUT, REPORT, UPDATE, CANCELUPLOAD, HEAD, ' +
+      'OPTIONS, GET, POST',
+    '/_config/cors/headers':
+      'Cache-Control, Content-Type, Depth, Destination, ' +
+        'If-Modified-Since, Overwrite, User-Agent, X-File-Name, ' +
+        'X-File-Size, X-Requested-With, accept, accept-encoding, ' +
+        'accept-language, authorization, content-type, origin, referer'
+  };
+
+  Promise.all(Object.keys(corsValues).map(function (key) {
+      var value = corsValues[key];
+      return request({
+        method: 'put',
+        url: COUCH_HOST + key,
+        body: JSON.stringify(value)
+      });
+    })).then(function () {
+      http_server.createServer().listen(HTTP_PORT, function () {
+        serversStarted = true;
+        printServerInfo();
+        checkReady();
+      });
+    }).catch(function (err) {
+      if (err) {
+        console.log(err);
+        process.exit(1);
+      }
+    });
+}
+
+function startCorsProxy() {
+  console.log('Starting up CORS proxy at localhost:2020');
   http_server.createServer().listen(HTTP_PORT, function () {
     cors_proxy.options = {target: COUCH_HOST};
     http_proxy.createServer(cors_proxy).listen(CORS_PORT, function () {
-      var testRoot = 'http://127.0.0.1:' + HTTP_PORT;
-      var query = '';
-      Object.keys(queryParams).forEach(function (key) {
-        query += (query ? '&' : '?');
-        query += key + '=' + encodeURIComponent(queryParams[key]);
-      });
-      console.log('Integration tests: ' + testRoot +
-        '/tests/test.html' + query);
-      console.log('Performance tests: ' + testRoot +
-        '/tests/performance/test.html');
       serversStarted = true;
+      printServerInfo();
       checkReady();
     });
   });
+}
+
+function startServers(callback) {
+  readyCallback = callback;
+  if (process.env.CORS_PROXY === 'true') {
+    startCorsProxy();
+  } else {
+    setUpCors();
+  }
 }
 
 function checkReady() {
