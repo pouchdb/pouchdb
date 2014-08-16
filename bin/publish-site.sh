9e7455f373cdd9825d0d4924320db1f60bba0857
Dale Harvey <dale@arandomurl.com>
2014-08-16 11:24:46 -0700

    (#2625) - Update publish site script to use server

--- bin/publish-site.sh
+++ bin/publish-site.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-# Store current branch hash
-CRT=`git rev-parse --abbrev-ref HEAD`
-
 # Build the website
 BUILD=1 npm run build-site
 
-# Checkout gh-pages and clean everything up
-git checkout gh-pages
-
-# Copy the docs in and git add them, we need to make sure if
-# any more top level parts directories get introduced they are
-# added here
-cp -R docs/_site/* ./
-echo "pouchdb.com" > CNAME
-
-git add CNAME *.html static 2014
-
-# Push updates
-git commit -m "Site Update"
-git push origin gh-pages
-
-# Go back to our old branch
-git checkout $CRT
+# Push the site live, requires credentials, open a bug
+# if you need to be able to push the site
+scp -r docs/_site/* pouchdb@pouchdb.com:/home/pouchdb/www/pouchdb.com/
