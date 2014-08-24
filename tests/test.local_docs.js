4111c319c27fd901c0abbbdc62b06fd9b6826309
Nolan Lawson <nolan.lawson@gmail.com>
2014-08-24 12:47:31 -0400

    (#2677) - local docs start at 0-1, don't save _revisions
    Two things:
    * local docs should start at 0-1, not 0-0. This is not strictly
      necessary to solve the bug, but I find it easier to understand
      when we do exactly the same thing as CouchDB.
    * CouchDB tries to "help" us by also giving us a revision object
      (e.g. `"_revisions":{"start":0,"ids":["5"]}` for rev `'0-5'`),
      which we save ver batim and then return back when you do a GET
      on that same local doc. That confuses CouchDB and it ends up
      giving us the wrong _rev the next time it PUTs the doc.

--- tests/test.local_docs.js
+++ tests/test.local_docs.js
@@ -26,6 +26,27 @@ adapters.forEach(function (adapter) {
       });
     });
 
+    it('local docs - put then get w/ revisions', function () {
+      var db = new PouchDB(dbs.name);
+      var doc = {
+        _id: '_local/foo'
+      };
+      return db.put(doc).then(function (res) {
+        res.id.should.equal('_local/foo');
+        res.rev.should.be.a('string');
+        res.ok.should.equal(true);
+        return db.get('_local/foo');
+      }).then(function (doc) {
+        should.not.exist(doc._revisions);
+        doc._revisions = {start: 0, ids: ['1']};
+        return db.put(doc);
+      }).then(function () {
+        return db.get('_local/foo');
+      }).then(function (doc) {
+        should.not.exist(doc._revisions);
+      });
+    });
+
     it('local docs - put then remove then get', function () {
       var db = new PouchDB(dbs.name);
       var doc = {_id: '_local/foo'};
@@ -56,6 +77,27 @@ adapters.forEach(function (adapter) {
         res.ok.should.equal(true);
         delete doc._rev;
         return db.put(doc);
+      });
+    });
+
+    it('local docs - put after remove, check return vals', function () {
+      // as long as it starts with 0-, couch
+      // treats it as a new local doc
+      var db = new PouchDB(dbs.name);
+      var doc = {_id: '_local/quux'};
+      return db.put(doc).then(function (res) {
+        res.rev.should.equal('0-1');
+        res.ok.should.equal(true);
+        doc._rev = res.rev;
+        return db.put(doc);
+      }).then(function (res) {
+        res.rev.should.equal('0-2');
+        res.ok.should.equal(true);
+        doc._rev = res.rev;
+        return db.put(doc);
+      }).then(function (res) {
+        res.rev.should.equal('0-3');
+        res.ok.should.equal(true);
       });
     });
 
