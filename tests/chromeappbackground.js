801810444b415f2d8ff67a9459421b923b5df064
Nolan Lawson <nolan.lawson@gmail.com>
2014-08-29 21:55:23 -0400

    build 3.0.3

--- tests/chromeappbackground.js
+++
@@ -1,8 +0,0 @@
-/*globals chrome */
-'use strict';
-chrome.app.runtime.onLaunched.addListener(function () {
-  chrome.app.window.create('./test.html', {
-    'width': 1000,
-    'height': 800
-  });
-});
