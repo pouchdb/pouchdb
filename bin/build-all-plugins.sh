7f4d88c14edaa0ab48726eb9345d0c3d0c32c490
Calvin Metcalf <cmetcalf@appgeo.com>
2014-09-29 10:44:09 -0400

    test redblackdown

--- bin/build-all-plugins.sh
+++ bin/build-all-plugins.sh
@@ -12,8 +12,13 @@ LEVEL_BACKEND=level-js \
   OUTPUT_FILENAME=pouchdb.idb-alt.js \
   ./bin/build-plugin.sh
 
+LEVEL_BACKEND=redblackdown \
+  OUTPUT_FILENAME=pouchdb.redblack.js \
+  ./bin/build-plugin.sh
+
 UGLIFY=./node_modules/uglify-js/bin/uglifyjs
 
 $UGLIFY dist/pouchdb.memory.js -mc > dist/pouchdb.memory.min.js
 $UGLIFY dist/pouchdb.localstorage.js -mc > dist/pouchdb.localstorage.min.js
 $UGLIFY dist/pouchdb.idb-alt.js -mc > dist/pouchdb.idb-alt.min.js
+$UGLIFY dist/pouchdb.redblack.js -mc > dist/pouchdb.redblack.min.js
