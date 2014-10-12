87a674e93bd459877d3abd05dd104837197efa2e
Dale Harvey <dale@arandomurl.com>
2014-10-12 16:35:27 +0200

    (#2850) - Run tests against CouchDB master (cluster)

---
+++ bin/run-couch-master-on-travis.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CWD=$(pwd)
+
+# Install deps
+sudo apt-get --no-install-recommends -y install \
+    build-essential \
+    ca-certificates \
+    curl \
+    erlang-dev \
+    erlang-nox \
+    git \
+    libicu-dev \
+    libmozjs185-dev \
+    python
+
+# Rebar isnt in apt
+git clone git://github.com/rebar/rebar.git ~/rebar
+cd ~/rebar
+./bootstrap
+sudo cp ./rebar /usr/local/bin
+
+# Sweet, build CouchDB
+cd ..
+git clone https://github.com/apache/couchdb.git ~/couchdb
+cd ~/couchdb
+./configure && make
+
+# All done, run a cluster
+python dev/run &
+
+# Lets get rid of this at soem point :)
+sleep 5
+
+cd $CWD
