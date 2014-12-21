b1c85e285f66f042a450a3a96eca9ba79f0b3053
Nolan Lawson <nolan.lawson@gmail.com>
2014-12-21 17:58:18 -0500

    more tests

--- packages/node_modules/pouchdb-find/test/test.js
+++ packages/node_modules/pouchdb-find/test/test.js
@@ -158,5 +158,132 @@ function tests(dbName, dbType) {
         });
       });
     });
+
+    it('sorts correctly', function () {
+      var index = {
+        "index": {
+          "fields": ["foo"]
+        },
+        "name": "foo-index",
+        "type": "json"
+      };
+      return db.createIndex(index).then(function () {
+        return db.bulkDocs([
+          {
+            _id: '1',
+            foo: 'AAA'
+          }, {
+            _id: '2',
+            foo: 'aAA'
+          }, {
+            _id: '3',
+            foo: 'BAA'
+          }, {
+            _id: '4',
+            foo: 'bAA'
+          }, {
+            _id: '5',
+            foo: '\u0000aAA'
+          }, {
+            _id: '6',
+            foo: '\u0001AAA'
+          }
+        ]);
+      }).then(function () {
+        return db.find({
+          "selector": {"foo": {"$gt": "\u0000\u0000"}},
+          "fields": ["_id", "foo"],
+          "sort": [{"foo": "asc"}]
+        });
+      }).then(function (resp) {
+        // ASCII vs ICU ordering. just gonna hack this
+        if (dbType === 'http') {
+          resp.should.deep.equal({
+            "docs": [
+              { "_id": "2", "foo": "aAA"},
+              { "_id": "5", "foo": "\u0000aAA"},
+              { "_id": "1", "foo": "AAA"},
+              { "_id": "6", "foo": "\u0001AAA"},
+              { "_id": "4", "foo": "bAA"},
+              { "_id": "3", "foo": "BAA"}
+            ]
+          });
+        } else {
+          resp.should.deep.equal({
+            docs: [
+              { _id: '5', foo: '\u0000aAA' },
+              { _id: '6', foo: '\u0001AAA' },
+              { _id: '1', foo: 'AAA' },
+              { _id: '3', foo: 'BAA' },
+              { _id: '2', foo: 'aAA' },
+              { _id: '4', foo: 'bAA' }
+            ]
+          });
+        }
+      });
+    });
+
+    it('error: conflicting sort and selector', function () {
+      var index = {
+        "index": {
+          "fields": ["foo"]
+        },
+        "name": "foo-index",
+        "type": "json"
+      };
+      return db.createIndex(index).then(function () {
+        return db.find({
+          "selector": {"foo": {"$gt": "\u0000\u0000"}},
+          "fields": ["_id", "foo"],
+          "sort": [{"_id": "asc"}]
+        });
+      }).then(function () {
+        throw new Error('shouldnt be here');
+      }, function (err) {
+        should.exist(err);
+      });
+    });
+
+    it('error - no selector', function () {
+      var index = {
+        "index": {
+          "fields": ["foo"]
+        },
+        "name": "foo-index",
+        "type": "json"
+      };
+      return db.createIndex(index).then(function () {
+        return db.find({
+          "fields": ["_id", "foo"],
+          "sort": [{"foo": "asc"}]
+        });
+      }).then(function () {
+        throw new Error('shouldnt be here');
+      }, function (err) {
+        should.exist(err);
+      });
+    });
+
+    it('error - no usable index', function () {
+      var index = {
+        "index": {
+          "fields": ["foo"]
+        },
+        "name": "foo-index",
+        "type": "json"
+      };
+      return db.createIndex(index).then(function () {
+        return db.find({
+          "selector": {"foo": "$exists"},
+          "fields": ["_id", "foo"],
+          "sort": [{"bar": "asc"}]
+        });
+      }).then(function () {
+        throw new Error('shouldnt be here');
+      }, function (err) {
+        should.exist(err);
+      });
+    });
+
   });
 }
