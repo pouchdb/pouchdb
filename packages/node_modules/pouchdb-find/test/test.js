4a5547ec9fbc225e19fdd73fd4d2fbe2359ce397
Nolan Lawson <nolan.lawson@gmail.com>
2014-12-21 16:48:50 -0500

    more work

--- packages/node_modules/pouchdb-find/test/test.js
+++ packages/node_modules/pouchdb-find/test/test.js
@@ -33,30 +33,130 @@ function tests(dbName, dbType) {
   var db;
 
   beforeEach(function () {
+    this.timeout(20000);
     db = new Pouch(dbName);
     return db;
   });
   afterEach(function () {
+    this.timeout(20000);
     return Pouch.destroy(dbName);
   });
   describe(dbType + ' tests', function () {
+    this.timeout(100000);
 
     it('should create an index', function () {
       var index = {
         "index": {
           "fields": ["foo"]
         },
-        "name" : "foo-index",
-        "type" : "json"
+        "name": "foo-index",
+        "type": "json"
       };
       return db.createIndex(index).then(function (response) {
-        should.exist(response);
-        response.should.deep.equal({"result":"created"});
+        response.should.deep.equal({"result": "created"});
         return db.createIndex(index);
       }).then(function (response) {
-        response.should.deep.equal({ result: 'exists' });
+        response.should.deep.equal({result: 'exists'});
       });
     });
 
+    it('should find existing indexes', function () {
+
+      return db.getIndexes().then(function (response) {
+        response.should.deep.equal({
+          indexes: [{
+            ddoc: null,
+            name: '_all_docs',
+            type: 'special',
+            def: {fields: [{_id: 'asc'}]}
+          }]
+        });
+        var index = {
+          "index": {
+            "fields": ["foo"]
+          },
+          "name": "foo-index",
+          "type": "json"
+        };
+        return db.createIndex(index);
+      }).then(function () {
+        return db.getIndexes();
+      }).then(function (resp) {
+        var ddoc = resp.indexes[1].ddoc;
+        ddoc.should.match(/_design\/.+/);
+        delete resp.indexes[1].ddoc;
+        resp.should.deep.equal({
+          "indexes": [
+            {
+              "ddoc": null,
+              "name": "_all_docs",
+              "type": "special",
+              "def": {
+                "fields": [
+                  {
+                    "_id": "asc"
+                  }
+                ]
+              }
+            },
+            {
+              //"ddoc": "_design/a5f4711fc9448864a13c81dc71e660b524d7410c",
+              "name": "foo-index",
+              "type": "json",
+              "def": {
+                "fields": [
+                  {
+                    "foo": "asc"
+                  }
+                ]
+              }
+            }
+          ]
+        });
+      });
+    });
+
+    it('should create ddocs automatically', function () {
+      var index = {
+        "index": {
+          "fields": ["foo"]
+        },
+        "name": "foo-index",
+        "type": "json"
+      };
+      var ddocId;
+      return db.createIndex(index).then(function () {
+        return db.getIndexes();
+      }).then(function (resp) {
+        ddocId = resp.indexes[1].ddoc;
+        return db.get(ddocId);
+      }).then(function (ddoc) {
+        ddoc._id.should.equal(ddocId);
+        should.exist(ddoc._rev);
+        delete ddoc._id;
+        delete ddoc._rev;
+        delete ddoc.views['foo-index'].options.w; // wtf is this?
+        ddoc.should.deep.equal({
+          "language": "query",
+          "views": {
+            "foo-index": {
+              "map": {
+                "fields": {
+                  "foo": "asc"
+                }
+              },
+              "reduce": "_count",
+              "options": {
+                "def": {
+                  "fields": [
+                    "foo"
+                  ]
+                }
+              }
+            }
+          }
+        });
+      });
+    });
   });
 }
