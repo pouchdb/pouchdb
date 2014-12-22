85221f5153d217df893cdeb09790c45d8e116bcc
Nolan Lawson <nolan.lawson@gmail.com>
2014-12-21 23:48:08 -0500

    add skipped query

--- packages/node_modules/pouchdb-find/test/test.js
+++ packages/node_modules/pouchdb-find/test/test.js
@@ -392,6 +392,39 @@ function tests(dbName, dbType) {
       });
     });
 
+    it.skip('does ne queries', function () {
+      var index = {
+        "index": {
+          "fields": ["foo"]
+        },
+        "name": "foo-index",
+        "type": "json"
+      };
+
+      return db.createIndex(index).then(function () {
+        return db.bulkDocs([
+          { _id: '1', foo: 'eyo'},
+          { _id: '2', foo: 'ebb'},
+          { _id: '3', foo: 'eba'},
+          { _id: '4', foo: 'abo'}
+        ]);
+      }).then(function () {
+        return db.find({
+          selector: {foo: {$ne: "eba"}},
+          fields: ["_id", "foo"],
+          sort: [{foo: "asc"}]
+        });
+      }).then(function (resp) {
+        resp.should.deep.equal({
+          docs: [
+            { _id: '4', foo: 'abo'},
+            { _id: '2', foo: 'ebb'},
+            { _id: '1', foo: 'eyo'}
+          ]
+        });
+      });
+    });
+
     it('does eq queries, no fields', function () {
       var index = {
         "index": {
