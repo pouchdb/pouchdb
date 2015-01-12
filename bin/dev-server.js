4cce3c6af81f2940afa777c387cad867c682e008
Chris Anderson <jchris@couchbase.com>
2015-01-12 13:15:25 -0800

    Serve admin party config to Sync Gateway.
    This requires that CSG be configured to look for database config from
    the HTTP service that the dev server starts on port 8001.
    
    Example Sync Gateway config: https://github.com/couchbase/sync_gateway/blob/master/examples/config_server.json

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -9,6 +9,7 @@ var _derequire = require('derequire');
 var watchify = require('watchify');
 var browserify = require('browserify');
 var cors_proxy = require('corsproxy');
+var http = require('http');
 var http_proxy = require('pouchdb-http-proxy');
 var http_server = require('http-server');
 var mkdirp = require('mkdirp');
@@ -93,12 +94,27 @@ var COUCH_HOST = process.env.COUCH_HOST || 'http://127.0.0.1:5984';
 
 var HTTP_PORT = 8000;
 var CORS_PORT = 2020;
+var SG_CONFIG_PORT = 8001;
+
+var sgDBConfig = {
+    "server": "walrus:",
+    "users": {
+      "GUEST": {"disabled": false, "admin_channels": ["*"] }
+    }
+  }
+
+function sgConfig(req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  console.log("sgDBConfig",req.url)
+  res.end(JSON.stringify(sgDBConfig));
+}
 
 var serversStarted;
 var readyCallback;
 
 function startServers(callback) {
   readyCallback = callback;
+  http.createServer(sgConfig).listen(SG_CONFIG_PORT);
   http_server.createServer().listen(HTTP_PORT, function () {
     cors_proxy.options = {target: COUCH_HOST};
     http_proxy.createServer(cors_proxy).listen(CORS_PORT, function () {
