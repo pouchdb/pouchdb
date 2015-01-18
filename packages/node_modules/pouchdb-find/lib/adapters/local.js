32e368d3f223306301c50692e88ab7df4c9ba7ab
Nolan Lawson <nolan.lawson@gmail.com>
2015-01-17 21:25:24 -0500

    hey, one test passing now

--- packages/node_modules/pouchdb-find/lib/adapters/local.js
+++ packages/node_modules/pouchdb-find/lib/adapters/local.js
@@ -1,22 +1,37 @@
 'use strict';
 
-function createIndex(db, requestDef, callback) {
-  callback(null, {not: 'implemented'});
+var utils = require('../utils');
+var upsert = require('pouchdb-upsert');
+var callbackify = utils.callbackify;
+
+function putIfNotExists(db, doc) {
+  return upsert.putIfNotExists.call(db, doc);
 }
 
-function find(db, requestDef, callback) {
-  callback(null, {not: 'implemented'});
+function createIndex(db, requestDef) {
+
+  var md5 = utils.MD5(JSON.stringify(requestDef));
+
+  return putIfNotExists(db, {
+    _id: '_design/' + md5
+  }).then(function (res) {
+    return {result: res.updated ? 'created' : 'exists'};
+  });
 }
 
-function getIndexes(db, callback) {
-  callback(null, {not: 'implemented'});
+function find(db, requestDef) {
+  throw new Error('not implemented');
 }
 
-function deleteIndex(db, indexDef, callback) {
-  callback(null, {not: 'implemented'});
+function getIndexes(db) {
+  throw new Error('not implemented');
 }
 
-exports.createIndex = createIndex;
-exports.find = find;
-exports.getIndexes = getIndexes;
-exports.deleteIndex = deleteIndex;
+function deleteIndex(db, indexDef) {
+  throw new Error('not implemented');
+}
+
+exports.createIndex = callbackify(createIndex);
+exports.find = callbackify(find);
+exports.getIndexes = callbackify(getIndexes);
+exports.deleteIndex = callbackify(deleteIndex);
