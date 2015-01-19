1ca16568ff685ab40d0b830be48b0c080413f546
Nolan Lawson <nolan.lawson@gmail.com>
2015-01-18 19:55:27 -0500

    start work on multi-fields

--- packages/node_modules/pouchdb-find/test/test.js
+++ packages/node_modules/pouchdb-find/test/test.js
@@ -841,6 +841,70 @@ function tests(dbName, dbType) {
       });
     });
 
+    it('supported mixed sort', function () {
+      var index = {
+        "index": {
+          "fields": [
+            "foo",
+            "bar"
+          ]
+        },
+        "name": "foo-index",
+        "type": "json"
+      };
+      return db.createIndex(index).then(function () {
+        return db.bulkDocs([
+          {_id: 'a1', foo: 'a', bar: '1'},
+          {_id: 'a2', foo: 'a', bar: '2'},
+          {_id: 'b1', foo: 'b', bar: '1'}
+        ]);
+      }).then(function () {
+        return db.find({
+          selector: {foo: {$gte: 'a'}}
+        });
+      }).then(function (res) {
+        res.docs.forEach(function (doc) {
+          should.exist(doc._rev);
+          delete doc._rev;
+        });
+        res.should.deep.equal({
+          "docs": [
+            {
+              "_id": "a1",
+              "foo": "a",
+              "bar": "1"
+            },
+            {
+              "_id": "a2",
+              "foo": "a",
+              "bar": "2"
+            },
+            {
+              "_id": "b1",
+              "foo": "b",
+              "bar": "1"
+            }
+          ]
+        });
+      });
+    });
+
+    it('error: gimme some args', function () {
+      return db.find().then(function () {
+        throw Error('should not be here');
+      }, function (err) {
+        should.exist(err);
+      });
+    });
+
+    it('error: missing required key selector', function () {
+      return db.find({}).then(function () {
+        throw Error('should not be here');
+      }, function (err) {
+        should.exist(err);
+      });
+    });
+
     it('error: unsupported mixed sort', function () {
       var index = {
         "index": {
