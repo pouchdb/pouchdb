3c9caf28963da9281805f1bd6c42617525e1182d
Nolan Lawson <nolan.lawson@gmail.com>
2015-01-19 17:20:19 -0500

    progress

--- packages/node_modules/pouchdb-find/lib/adapters/local/query-planner.js
+++ packages/node_modules/pouchdb-find/lib/adapters/local/query-planner.js
@@ -11,17 +11,44 @@ function planQuery(selector, indexes) {
   if (typeof matcher === 'string') {
     matcher = {$eq: matcher};
   }
-  matcher = {
-    operator: getKey(matcher),
-    value: getValue(matcher)
-  };
+  var userOperator = getKey(matcher);
+  var userValue = getValue(matcher);
+  var queryOpts = {};
 
-  var res = {field: field, matcher: matcher};
+  var res = {field: field, queryOpts: queryOpts};
 
   indexes.forEach(function (index) {
     var indexField = getKey(index.def.fields[0]);
     if (indexField === field) {
+      // found a good index
       res.index = index;
+      if (index.def.fields.length > 1) {
+        // index has 2 fields, but user is only looking at first one
+        if (userOperator === '$eq') {
+          queryOpts.startkey = [userValue];
+          queryOpts.endkey = [userValue, {}, {}];
+        }
+      } else { // same number of fields
+        switch (userOperator) {
+          case '$eq':
+            queryOpts.key = userValue;
+            break;
+          case '$lte':
+            queryOpts.endkey = userValue;
+            break;
+          case '$gte':
+            queryOpts.startkey = userValue;
+            break;
+          case '$lt':
+            queryOpts.endkey = userValue;
+            queryOpts.inclusive_end = false;
+            break;
+          case '$gt':
+            queryOpts.startkey = userValue;
+            queryOpts.inclusive_start = false;
+            break;
+        }
+      }
     }
   });
 
