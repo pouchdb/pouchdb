63f78ada66b52e045ebf092136f230a909282077
Nolan Lawson <nolan.lawson@gmail.com>
2015-01-24 20:12:12 -0500

    index deep fields

--- packages/node_modules/pouchdb-find/lib/adapters/local/abstract-mapper.js
+++ packages/node_modules/pouchdb-find/lib/adapters/local/abstract-mapper.js
@@ -2,21 +2,118 @@
 
 var abstractMapReduce = require('pouchdb-abstract-mapreduce');
 
-function createMapper(fields, asc, emit) {
-  if (fields.length === 1) {
-    // optimization for the simplest case
-    var field = fields[0];
-    return function indexesMapFun(doc) {
-      emit(doc[field]);
-    };
-  } else {
-    return function indexesMapFun(doc) {
-      var toEmit = [];
-      for (var i = 0, len = fields.length; i < len; i++) {
-        toEmit.push(doc[fields[i]]);
+//
+// One thing about these mappers:
+//
+// Per the advice of John-David Dalton (http://youtu.be/NthmeLEhDDM),
+// what you want to do in this case is optimize for the smallest possible
+// function, since that's the thing that gets run over and over again.
+//
+// This code would be a lot simpler if all the if/elses were inside
+// the function, but it would also be a lot less performant.
+//
+
+function parseField(fieldName) {
+  // fields may be deep (e.g. "foo.bar.baz"), so parse
+  var fields = [];
+  var current = '';
+  for (var i = 0, len = fieldName.length; i < len; i++) {
+    var ch = fieldName[i];
+    if (ch === '.') {
+      if (i > 0 && fieldName[i - 1] === '\\') { // escaped delimiter
+        current = current.substring(0, current.length - 1) + '.';
+      } else { // not escaped, so delimiter
+        fields.push(current);
+        current = '';
       }
-      emit(toEmit);
-    };
+    } else { // normal character
+      current += ch;
+    }
+  }
+  if (current) {
+    fields.push(current);
+  }
+  return fields;
+}
+
+function createDeepMultiMapper(fields, emit) {
+  return function (doc) {
+    var toEmit = [];
+    for (var i = 0, iLen = fields.length; i < iLen; i++) {
+      var parsedField = parseField(fields[i]);
+      var value = doc;
+      for (var j = 0, jLen = parsedField.length; j < jLen; j++) {
+        var key = parsedField[j];
+        value = value[key];
+        if (!value) {
+          break;
+        }
+      }
+      toEmit.push(value);
+    }
+    emit(toEmit);
+  };
+}
+
+function createDeepSingleMapper(field, emit) {
+  var parsedField = parseField(field);
+  return function (doc) {
+    var value = doc;
+    for (var i = 0, len = parsedField.length; i < len; i++) {
+      var key = parsedField[i];
+      value = value[key];
+      if (!value) {
+        return; // do nothing
+      }
+    }
+    emit(value);
+  };
+}
+
+function createShallowSingleMapper(field, emit) {
+  return function (doc) {
+    emit(doc[field]);
+  };
+}
+
+function createShallowMultiMapper(fields, emit) {
+  return function (doc) {
+    var toEmit = [];
+    for (var i = 0, len = fields.length; i < len; i++) {
+      toEmit.push(doc[fields[i]]);
+    }
+    emit(toEmit);
+  };
+}
+
+function checkShallow(fields) {
+  for (var i = 0, len = fields.length; i < len; i++) {
+    var field = fields[i];
+    if (field.indexOf('.') !== -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function createMapper(fields, emit) {
+  var isShallow = checkShallow(fields);
+  var isSingle = fields.length === 1;
+
+  // notice we try to optimize for the most common case,
+  // i.e. single shallow indexes
+  if (isShallow) {
+    if (isSingle) {
+      return createShallowSingleMapper(fields[0], emit);
+    } else { // multi
+      return createShallowMultiMapper(fields, emit);
+    }
+  } else { // deep
+    if (isSingle) {
+      return createDeepSingleMapper(fields[0], emit);
+    } else { // multi
+      return createDeepMultiMapper(fields, emit);
+    }
   }
 }
 
@@ -26,10 +123,8 @@ var abstractMapper = abstractMapReduce({
     // mapFunDef is a list of fields
 
     var fields = Object.keys(mapFunDef.fields);
-    // either all asc or all desc
-    var asc = mapFunDef.fields[fields[0]] === 'asc';
 
-    return createMapper(fields, asc, emit);
+    return createMapper(fields, emit);
   },
   reducer: function (/*reduceFunDef*/) {
     throw new Error('reduce not supported');
