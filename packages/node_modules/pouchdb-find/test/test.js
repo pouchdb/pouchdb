eb9ea66d74d6f03136e3daa702ab45ae1de1d68c
Nolan Lawson <nolan.lawson@gmail.com>
2015-01-24 21:40:21 -0500

    implement $exists

--- packages/node_modules/pouchdb-find/test/test.js
+++ packages/node_modules/pouchdb-find/test/test.js
@@ -1218,6 +1218,187 @@ function tests(dbName, dbType) {
       });
     });
 
+    //
+    // TODO: cloudant seems to have implemented these incorrectly
+    //
+    if (dbType === 'local') {
+      it('does $exists queries - true', function () {
+        var index = {
+          "index": {
+            "fields": ["foo"]
+          },
+          "name": "foo-index",
+          "type": "json"
+        };
+        return db.createIndex(index).then(function () {
+          return db.bulkDocs([
+            {_id: 'a', foo: 'bar'},
+            {_id: 'b', foo: {yo: 'dude'}},
+            {_id: 'c', foo: null},
+            {_id: 'd'}
+          ]);
+        }).then(function () {
+          return db.find({
+            selector: {'foo': {'$exists': true}},
+            fields: ['_id']
+          });
+        }).then(function (res) {
+          res.docs.sort(sortById);
+          res.docs.should.deep.equal([
+            { "_id": "a" },
+            { "_id": "b" }
+          ]);
+        });
+      });
+
+      it('does $exists queries - false', function () {
+        var index = {
+          "index": {
+            "fields": ["foo"]
+          },
+          "name": "foo-index",
+          "type": "json"
+        };
+        return db.createIndex(index).then(function () {
+          return db.bulkDocs([
+            {_id: 'a', foo: 'bar'},
+            {_id: 'b', foo: {yo: 'dude'}},
+            {_id: 'c', foo: null},
+            {_id: 'd'}
+          ]);
+        }).then(function () {
+          return db.find({
+            selector: {'foo': {'$exists': false}},
+            fields: ['_id']
+          });
+        }).then(function (res) {
+          res.docs.sort(sortById);
+          res.docs.should.deep.equal([
+            { "_id": "c" },
+            { "_id": "d" },
+          ]);
+        });
+      });
+
+      it('does $exists queries - true/undef (multi-field)', function () {
+        var index = {
+          "index": {
+            "fields": ["foo", "bar"]
+          },
+          "name": "foo-index",
+          "type": "json"
+        };
+        return db.createIndex(index).then(function () {
+          return db.bulkDocs([
+            {_id: 'a', foo: 'bar', bar: 'baz'},
+            {_id: 'b', foo: {yo: 'dude'}},
+            {_id: 'c', foo: null, bar: 'quux'},
+            {_id: 'd'}
+          ]);
+        }).then(function () {
+          return db.find({
+            selector: {'foo': {'$exists': true}},
+            fields: ['_id']
+          });
+        }).then(function (res) {
+          res.docs.sort(sortById);
+          res.docs.should.deep.equal([
+            { "_id": "a" },
+            { "_id": "b" }
+          ]);
+        });
+      });
+
+      it('does $exists queries - $eq/true (multi-field)', function () {
+        var index = {
+          "index": {
+            "fields": ["foo", "bar"]
+          },
+          "name": "foo-index",
+          "type": "json"
+        };
+        return db.createIndex(index).then(function () {
+          return db.bulkDocs([
+            {_id: 'a', foo: 'bar', bar: 'baz'},
+            {_id: 'b', foo: 'bar', bar: {yo: 'dude'}},
+            {_id: 'c', foo: null, bar: 'quux'},
+            {_id: 'd'}
+          ]);
+        }).then(function () {
+          return db.find({
+            selector: {'foo': 'bar', bar: {$exists: true}},
+            fields: ['_id']
+          });
+        }).then(function (res) {
+          res.docs.sort(sortById);
+          res.docs.should.deep.equal([
+            { "_id": "a" },
+            { "_id": "b" }
+          ]);
+        });
+      });
+
+      // TODO: multi-field $exists is hard
+      it.skip('does $exists queries - true/true (multi-field)', function () {
+        var index = {
+          "index": {
+            "fields": ["foo", "bar"]
+          },
+          "name": "foo-index",
+          "type": "json"
+        };
+        return db.createIndex(index).then(function () {
+          return db.bulkDocs([
+            {_id: 'a', foo: 'bar', bar: 'baz'},
+            {_id: 'b', foo: {yo: 'dude'}},
+            {_id: 'c', foo: null, bar: 'quux'},
+            {_id: 'd'}
+          ]);
+        }).then(function () {
+          return db.find({
+            selector: {'foo': {'$exists': true}, 'bar': {$exists: true}},
+            fields: ['_id']
+          });
+        }).then(function (res) {
+          res.docs.sort(sortById);
+          res.docs.should.deep.equal([
+            { "_id": "a" }
+          ]);
+        });
+      });
+
+      // TODO: multi-field $exists is hard
+      it.skip('does $exists queries - true/false (multi-field)', function () {
+        var index = {
+          "index": {
+            "fields": ["foo", "bar"]
+          },
+          "name": "foo-index",
+          "type": "json"
+        };
+        return db.createIndex(index).then(function () {
+          return db.bulkDocs([
+            {_id: 'a', foo: 'bar', bar: 'baz'},
+            {_id: 'b', foo: {yo: 'dude'}},
+            {_id: 'c', foo: null, bar: 'quux'},
+            {_id: 'd'}
+          ]);
+        }).then(function () {
+          return db.find({
+            selector: {'foo': {'$exists': true}, 'bar': {$exists: false}},
+            fields: ['_id']
+          });
+        }).then(function (res) {
+          res.docs.sort(sortById);
+          res.docs.should.deep.equal([
+            { "_id": "b" }
+          ]);
+        });
+      });
+    }
+
+
+
     it.skip('does ne queries', function () {
       var index = {
         "index": {
