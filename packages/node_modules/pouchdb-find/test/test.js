cb40616941f18aa788d8d6f3509d054f75872249
Nolan Lawson <nolan.lawson@gmail.com>
2015-01-24 20:12:12 -0500

    index deep fields

--- packages/node_modules/pouchdb-find/test/test.js
+++ packages/node_modules/pouchdb-find/test/test.js
@@ -1131,6 +1131,93 @@ function tests(dbName, dbType) {
       });
     });
 
+    it('deep fields', function () {
+      var index = {
+        "index": {
+          "fields": [
+            "foo.bar"
+          ]
+        },
+        "name": "foo-index",
+        "type": "json"
+      };
+      return db.createIndex(index).then(function () {
+        return db.bulkDocs([
+          {_id: 'doc', foo: {bar: 'a'}},
+        ]);
+      }).then(function () {
+        return db.find({
+          selector: {'foo.bar': 'a'},
+          fields: ['_id']
+        });
+      }).then(function (res) {
+        res.should.deep.equal({
+          "docs": [
+            {
+              "_id": "doc"
+            }
+          ]
+        });
+      });
+    });
+
+    it('deeper fields', function () {
+      var index = {
+        "index": {
+          "fields": [
+            "foo.bar.baz"
+          ]
+        },
+        "name": "foo-index",
+        "type": "json"
+      };
+      return db.createIndex(index).then(function () {
+        return db.bulkDocs([
+          {_id: 'doc', foo: {bar: {baz: 'a'}}},
+        ]);
+      }).then(function () {
+        return db.find({
+          selector: {'foo.bar.baz': 'a'},
+          fields: ['_id']
+        });
+      }).then(function (res) {
+        res.should.deep.equal({
+          "docs": [
+            {
+              "_id": "doc"
+            }
+          ]
+        });
+      });
+    });
+
+    it('deep fields escaped', function () {
+      var index = {
+        "index": {
+          "fields": [
+            "foo\\.bar"
+          ]
+        },
+        "name": "foo-index",
+        "type": "json"
+      };
+      return db.createIndex(index).then(function () {
+        return db.bulkDocs([
+          {_id: 'doc1', foo: {bar: 'a'}},
+          {_id: 'doc2', 'foo.bar': 'a'}
+        ]);
+      }).then(function () {
+        return db.find({
+          selector: {'foo\\.bar': 'a'},
+          fields: ['_id']
+        });
+      }).then(function (res) {
+        res.should.deep.equal({
+          "docs": [{ "_id": "doc2"}]
+        });
+      });
+    });
+
     it.skip('does ne queries', function () {
       var index = {
         "index": {
