95341722186894a5126555b999018a12c5ee52ff
Chris Anderson <jchris@couchbase.com>
2015-02-10 10:16:39 -0800

    background the SG process

--- bin/run-csg-on-travis.sh
+++ bin/run-csg-on-travis.sh
@@ -10,7 +10,7 @@ curl -o csg.deb http://packages.couchbase.com/releases/couchbase-sync-gateway/1.
 sudo dpkg -i csg.deb
 
 # Run CSG
-/opt/couchbase-sync-gateway/bin/sync_gateway ./tests/misc/sync-gateway-config.json &
+/opt/couchbase-sync-gateway/bin/sync_gateway ./tests/misc/sync-gateway-config.json >sg.log 2>&1 < /dev/null &
 
 # Lets get rid of this at some point :)
 sleep 2
