301e30f7e9223a03f1a0bc2fb991f103e0e9987c
Nolan Lawson <nolan.lawson@gmail.com>
2015-02-15 23:02:54 -0500

    (#7) - implement $ne

--- packages/node_modules/pouchdb-find/test/tests/test.ne.js
+++ packages/node_modules/pouchdb-find/test/tests/test.ne.js
@@ -4,35 +4,63 @@ module.exports = function (dbType, context) {
 
   describe(dbType + ': ne', function () {
 
-    it.skip('does ne queries', function () {
+    it('#7 does ne queries 1', function () {
       var db = context.db;
       var index = {
         "index": {
           "fields": ["foo"]
-        },
-        "name": "foo-index",
-        "type": "json"
+        }
       };
 
       return db.createIndex(index).then(function () {
         return db.bulkDocs([
-          { _id: '1', foo: 'eyo'},
-          { _id: '2', foo: 'ebb'},
-          { _id: '3', foo: 'eba'},
-          { _id: '4', foo: 'abo'}
+          { _id: '1', foo: 'eyo', bar: 'zxy'},
+          { _id: '2', foo: 'ebb', bar: 'zxy'},
+          { _id: '3', foo: 'eba', bar: 'zxz'},
+          { _id: '4', foo: 'abo', bar: 'zxz'}
         ]);
       }).then(function () {
         return db.find({
-          selector: {foo: {$ne: "eba"}},
-          fields: ["_id", "foo"],
+          selector: {foo: {$gt: "a"}, bar: {$ne: 'zxy'}},
+          fields: ["_id"],
           sort: [{foo: "asc"}]
         });
       }).then(function (resp) {
         resp.should.deep.equal({
           docs: [
-            { _id: '4', foo: 'abo'},
-            { _id: '2', foo: 'ebb'},
-            { _id: '1', foo: 'eyo'}
+            { _id: '4'},
+            { _id: '3'}
+          ]
+        });
+      });
+    });
+
+    it('#7 does ne queries 2', function () {
+      var db = context.db;
+      var index = {
+        "index": {
+          "fields": ["foo", "bar"]
+        }
+      };
+
+      return db.createIndex(index).then(function () {
+        return db.bulkDocs([
+          {_id: '1', foo: 'eyo', bar: 'zxy'},
+          {_id: '2', foo: 'ebb', bar: 'zxy'},
+          {_id: '3', foo: 'eba', bar: 'zxz'},
+          {_id: '4', foo: 'abo', bar: 'zxz'}
+        ]);
+      }).then(function () {
+        return db.find({
+          selector: {foo: {$gt: "a"}, bar: {$ne: 'zxy'}},
+          fields: ["_id"],
+          sort: [{foo: "asc"}]
+        });
+      }).then(function (resp) {
+        resp.should.deep.equal({
+          docs: [
+            {_id: '4'},
+            {_id: '3'}
           ]
         });
       });
