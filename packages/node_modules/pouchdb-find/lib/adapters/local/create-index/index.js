d65beddb2b32116322aed4d2819609b123da6a43
Nolan Lawson <nolan.lawson@gmail.com>
2015-02-16 19:44:47 -0500

    (#11) - big refactor

---
+++ packages/node_modules/pouchdb-find/lib/adapters/local/create-index/index.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var utils = require('../../../utils');
+var log = utils.log;
+
+var pouchUpsert = require('pouchdb-upsert');
+var abstractMapper = require('../abstract-mapper');
+var localUtils = require('../utils');
+var validateIndex = localUtils.validateIndex;
+var massageIndexDef = localUtils.massageIndexDef;
+
+function upsert(db, docId, diffFun) {
+  return pouchUpsert.upsert.call(db, docId, diffFun);
+}
+
+function createIndex(db, requestDef) {
+
+  var originalIndexDef = utils.clone(requestDef.index);
+  requestDef.index = massageIndexDef(requestDef.index);
+
+  validateIndex(requestDef.index);
+
+  var md5 = utils.MD5(JSON.stringify(requestDef));
+
+  var viewName = requestDef.name || ('idx-' + md5);
+
+  var ddocName = requestDef.ddoc || ('idx-' + md5);
+  var ddocId = '_design/' + ddocName;
+
+  var hasInvalidLanguage = false;
+  var viewExists = false;
+
+  function updateDdoc(doc) {
+    if (doc._rev && doc.language !== 'query') {
+      hasInvalidLanguage = true;
+    }
+    doc.language = 'query';
+    doc.views = doc.views || {};
+
+    viewExists = !!doc.views[viewName];
+
+    doc.views[viewName] = {
+      map: {
+        fields: utils.mergeObjects(requestDef.index.fields)
+      },
+      reduce: '_count',
+      options: {
+        def: originalIndexDef
+      }
+    };
+
+    return doc;
+  }
+
+  log('creating index', ddocId);
+
+  return upsert(db, ddocId, updateDdoc).then(function () {
+    if (hasInvalidLanguage) {
+      throw new Error('invalid language for ddoc with id "' +
+      ddocId +
+      '" (should be "query")');
+    }
+  }).then(function () {
+    // kick off a build
+    // TODO: abstract-pouchdb-mapreduce should support auto-updating
+    // TODO: should also use update_after, but pouchdb/pouchdb#3415 blocks me
+    var signature = ddocName + '/' + viewName;
+    return abstractMapper.query.call(db, signature, {
+      limit: 0,
+      reduce: false
+    }).then(function () {
+      return {result: viewExists ? 'exists' : 'created'};
+    });
+  });
+}
+
+module.exports = createIndex;
--- packages/node_modules/pouchdb-find/lib/adapters/local/utils.js
+++ packages/node_modules/pouchdb-find/lib/adapters/local/utils.js
@@ -113,6 +113,29 @@ function validateFindRequest(requestDef) {
   }
 }
 
+function parseField(fieldName) {
+  // fields may be deep (e.g. "foo.bar.baz"), so parse
+  var fields = [];
+  var current = '';
+  for (var i = 0, len = fieldName.length; i < len; i++) {
+    var ch = fieldName[i];
+    if (ch === '.') {
+      if (i > 0 && fieldName[i - 1] === '\\') { // escaped delimiter
+        current = current.substring(0, current.length - 1) + '.';
+      } else { // not escaped, so delimiter
+        fields.push(current);
+        current = '';
+      }
+    } else { // normal character
+      current += ch;
+    }
+  }
+  if (current) {
+    fields.push(current);
+  }
+  return fields;
+}
+
 module.exports = {
   getKey: getKey,
   getValue: getValue,
@@ -123,5 +146,6 @@ module.exports = {
   validateFindRequest: validateFindRequest,
   reverseOptions: reverseOptions,
   filterInclusiveStart: filterInclusiveStart,
-  massageIndexDef: massageIndexDef
+  massageIndexDef: massageIndexDef,
+  parseField: parseField
 };
