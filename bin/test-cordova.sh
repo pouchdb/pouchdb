237a8960d88cd64c9e6bda4b81bea546be9de271
Nolan Lawson <nolan.lawson@gmail.com>
2015-04-20 23:27:41 -0400

    clean up emulator

--- bin/test-cordova.sh
+++ bin/test-cordova.sh
@@ -14,6 +14,7 @@ export APPIUM_SERVER_PID=$!
 echo "Starting emulator..."
 echo no | android create avd --force -n test -t android-19 --abi x86
 emulator -avd test -no-skin -no-audio -no-window &
+export EMULATOR_PID=$!
 ./bin/android-wait-for-emulator.sh
 adb shell input keyevent 82
 
@@ -26,5 +27,8 @@ if [[ ! -z $DEV_SERVER_PID ]]; then
 fi
 if [[ ! -z $APPIUM_SERVER_PID ]]; then
   kill $APPIUM_SERVER_PID
+fi
+if [[ ! -z $EMULATOR_PID ]]; then
+  kill $EMULATOR_PID
 fi
 exit $EXIT_STATUS
