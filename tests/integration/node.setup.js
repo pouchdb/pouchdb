22469fc8666963d9f2f342f7cb9f1b444c826aea
Dale Harvey <dale@arandomurl.com>
2015-06-21 23:40:02 +0100

    (#3945) - Use seedrandom to generate deterministic fuzzy tests

--- tests/integration/node.setup.js
+++ tests/integration/node.setup.js
@@ -3,6 +3,11 @@
 // throw an error if any EventEmitter adds too many listeners
 require('throw-max-listeners-error');
 
+var seedrandom = require('seedrandom');
+var seed = process.env.SEED || Date.now();
+console.log('Seeded with: ' + seed);
+seedrandom(seed, { global: true });
+
 var testsDir = process.env.TESTS_DIR || './tmp';
 var exec = require('child_process').exec;
 function cleanup() {
