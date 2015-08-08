deaf0e471b95bf900138bf350bb56e033d43c448
Nick Colley <nickcolley7@gmail.com>
2015-08-08 21:15:29 +0100

    (#4117) - Api page takes a long time to load

--- docs/static/js/code.js
+++ docs/static/js/code.js
@@ -1,70 +1,112 @@
-function codeWrap(){    
-  var codeTpl = '' +
-  '<ul class="nav nav-tabs nav-code" data-code-tablist>' +
-    '<li class="active">' +
-        '<a href="#promise">Promises</a>' +
-    '</li>' +
-    '<li>' +
-        '<a href="#callback">Callbacks</a>' +
-    '</li>' +
-  '</ul>' +
-  '<div class="tab-content">{{tapPanes}}</div>';
+/* jshint jquery: true */
 
-  var codeGroups =
-    $('[data-code-id]')
-      .get()
+function codeWrap(){
+  "use strict";
+
+  var DEFAULT_TYPE = 'promise';
+
+  var $codes = $('[data-code-id]').get();
+
+  var codeIds =
+    $codes
       .map(function(div){
           return div.attributes["data-code-id"].value
       })
       .filter(function(item, index, inputArray){
+          // Each code block has multiple versions so let's only grab one.
           return inputArray.indexOf(item) == index;
-      })
-      .forEach(function(id){
-        var $code = $("[data-code-id='" + id + "']");
-
-        var paneHeight = 0;
-
-        var paneHtml = $code.get().map(function(div){
-          if(div.clientHeight > paneHeight){
-            paneHeight = div.clientHeight;
-          }
-          return div.outerHTML;
-        }).join('');
-
-        // Pad the pane height
-        paneHeight = paneHeight + 15;
-
-        var codeHtml = codeTpl
-                         .replace(/{{tapPanes}}/g, paneHtml)
-                         .replace(/<pre>/g, "<pre style='height: " + paneHeight + "px;'");
-
-        $code
-          .first()
-          .replaceWith(codeHtml);
-        $code.remove();
-
-        // Remove items that are only useful for non-JS users.
-        $('[data-code-hide]').addClass('hide');
       });
+
+  wrap();
+  setAll();
 
   $('[data-code-tablist] [href]').on('click', function(e){
     var href = $(this).attr('href');
 
     setAll(href.replace('#', ''));
 
+    setEqualHeights();
+
     e.preventDefault();
   });
 
-  function setAll(type){
-    var type = type || localStorage.getItem('codeStyle');
-    if(typeof type === "undefined" || type === null) return;
+  function wrap(){
+    var codeTpl = '' +
+      '<ul class="nav nav-tabs nav-code" data-code-tablist="{{codeId}}">' +
+        '<li class="active">' +
+            '<a href="#promise">Promises</a>' +
+        '</li>' +
+        '<li>' +
+            '<a href="#callback">Callbacks</a>' +
+        '</li>' +
+      '</ul>' +
+      '<div class="tab-content">{{tapPanes}}</div>';
+    codeIds
+      .forEach(function(id){
+        var $code = $("[data-code-id='" + id + "']");
 
-    $('[data-code-tablist] li, .tab-pane').removeClass('active');
-    $('[data-code-tablist] [href="#' + type + '"]').parent().addClass('active');
+        var paneHtml = $code.get().map(function(div){
+          return div.outerHTML;
+        }).join('');
+
+        var codeHtml = codeTpl
+                          .replace(/{{tapPanes}}/g, paneHtml)
+                          .replace(/{{codeId}}/g, id);
+        $code
+          .first()
+          .replaceWith(codeHtml);
+        $code.remove();
+      });
+    // Remove items that are only useful for non-JS users.
+    $('[data-code-hide]').addClass('hide');
+  }
+
+  function setAll(type){
+
+    // We default to callback so no need to do anything the first time.
+    var firstTime = !localStorage.getItem('codeStyle');
+    if(firstTime){
+      localStorage.setItem('codeStyle', DEFAULT_TYPE);
+    }
+
+    type = type || localStorage.getItem('codeStyle');
+    if(typeof type === "undefined" || type === null) {
+      return;
+    }
+
+    var $tablist = $('[data-code-tablist] [href="#' + type + '"]').parent();
+    $('[data-code-tablist] li').removeClass('active');
+    $tablist.addClass('active');
+
+    $('.tab-pane').removeClass('active');
     $('.tab-pane[id="' + type + '"]').addClass('active');
     localStorage.setItem('codeStyle', type);
   }
 
-  setAll();
-};
+
+  var setHeights = [];
+  function setEqualHeights(){
+    if(setHeights.length > 0){
+      return;
+    }
+    codeIds
+      .forEach(function(id){
+        var $code = $("[data-code-id='" + id + "']");
+
+        var paneHeight = 0;
+
+        $code.get().forEach(function(div){
+          var originalDisplay = div.style.display;
+          div.style.display = 'block';
+          var clientHeight = div.clientHeight;
+          div.style.display = originalDisplay;
+          if(clientHeight > paneHeight){
+            paneHeight = clientHeight;
+          }
+        });
+        $code.find('pre').css('height', paneHeight);
+        setHeights.push(id);
+      });
+  }
+}
 codeWrap();
