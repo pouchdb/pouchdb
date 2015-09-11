5f3ed59e39a30c1c5a4f229f68c215111a1edcac
Nolan Lawson <nolan.lawson@gmail.com>
2015-09-11 08:28:27 -0400

    (#4231) - actually test CSG, without sudo

--- bin/run-csg-on-travis.sh
+++ bin/run-csg-on-travis.sh
@@ -4,17 +4,14 @@ set -x
 
 CWD=$(pwd)
 
-# TODO: fix this
-echo "Can't run sudo in Travis anymore; bailing"
-exit 1
-
 # Install CSG
 curl -o csg.deb http://packages.couchbase.com/releases/couchbase-sync-gateway/1.0.3/couchbase-sync-gateway-community_1.0.3_x86_64.deb
 
-sudo dpkg -i csg.deb
+dpkg --extract csg.deb csg
+cd csg/
 
 # Run CSG
-/opt/couchbase-sync-gateway/bin/sync_gateway ./tests/misc/sync-gateway-config.json >sg.log 2>&1 < /dev/null &
+./opt/couchbase-sync-gateway/bin/sync_gateway ../tests/misc/sync-gateway-config.json >sg.log 2>&1 < /dev/null &
 
 # Lets get rid of this at some point :)
 sleep 2
