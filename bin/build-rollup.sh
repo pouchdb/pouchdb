f53b80a944c69c2f96f23f4dc2083c2428174b76
Nolan Lawson <nolan.lawson@gmail.com>
2015-10-12 22:12:21 -0400

    more fixes

--- bin/build-rollup.sh
+++ bin/build-rollup.sh
@@ -10,6 +10,15 @@ done
 
 
 ./node_modules/.bin/rollup \
-  --name PouchDB --format umd \
+  --format cjs \
   --external events,inherits,argsarray,debug,double-ended-queue,es3ify,fruitdown,inherits,level-sublevel,level-write-stream,levelup,lie,localstorage-down,memdown,pouchdb-collate,pouchdb-collections,request,spark-md5,through2,vuvuzela \
-  lib_staging/index.js > dist/pouchdb.js
+  lib_staging/index.js > lib_staging/concated.js
+
+./node_modules/.bin/browserify \
+  lib_staging/concated \
+  -s PouchDB -p bundle-collapser/plugin \
+  | node ./bin/es3ify.js \
+  | ./node_modules/.bin/derequire > \
+  dist/pouchdb.js
+
+#rm -fr lib_staging
