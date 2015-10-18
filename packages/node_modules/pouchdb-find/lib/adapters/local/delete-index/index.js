e13bc5853c9c9ca79e9880850f2942538166852d
Nolan Lawson <nolan.lawson@gmail.com>
2015-10-18 13:25:22 -0400

    (#83) - fix deleteIndex coverage

--- packages/node_modules/pouchdb-find/lib/adapters/local/delete-index/index.js
+++ packages/node_modules/pouchdb-find/lib/adapters/local/delete-index/index.js
@@ -1,14 +1,32 @@
 'use strict';
 
 var abstractMapper = require('../abstract-mapper');
+var upsert = require('../../../abstract-mapreduce/upsert');
 
 function deleteIndex(db, index) {
 
-  var docId = index.ddoc;
+  if (!index.ddoc) {
+    throw new Error('you must supply an index.ddoc when deleting');
+  }
 
-  return db.get(docId).then(function (doc) {
-    return db.remove(doc);
-  }).then(function () {
+  if (!index.name) {
+    throw new Error('you must supply an index.name when deleting');
+  }
+
+  var docId = index.ddoc;
+  var viewName = index.name;
+
+  function deltaFun (doc) {
+    if (Object.keys(doc.views).length === 1 && doc.views[viewName]) {
+      // only one view in this ddoc, delete the whole ddoc
+      return {_id: docId, _deleted: true};
+    }
+    // more than one view here, just remove the view
+    delete doc.views[viewName];
+    return doc;
+  }
+
+  return upsert(db, docId, deltaFun).then(function () {
     return abstractMapper.viewCleanup.apply(db);
   }).then(function () {
     return {ok: true};
