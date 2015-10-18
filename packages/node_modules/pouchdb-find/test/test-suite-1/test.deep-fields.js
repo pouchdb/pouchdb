0de74baef48eb4db02ce4f5841a7b5cb69375108
Nolan Lawson <nolan.lawson@gmail.com>
2015-10-18 18:18:42 -0400

    (#83) - bunch more code coverage fixes

--- packages/node_modules/pouchdb-find/test/test-suite-1/test.deep-fields.js
+++ packages/node_modules/pouchdb-find/test/test-suite-1/test.deep-fields.js
@@ -93,5 +93,79 @@ module.exports = function (dbType, context) {
         });
       });
     });
+
+    it('should create a deep multi mapper', function () {
+      var db = context.db;
+      var index = {
+        "index": {
+          "fields": [
+            "foo.bar", "bar.baz"
+          ]
+        }
+      };
+      return db.createIndex(index).then(function () {
+        return db.bulkDocs([
+          {_id: 'a', foo: {bar: 'yo'}, bar: {baz: 'hey'}},
+          {_id: 'b', foo: {bar: 'sup'}, bar: {baz: 'dawg'}}
+        ]);
+      }).then(function () {
+        return db.find({
+          selector: {"foo.bar": 'yo', "bar.baz": 'hey'},
+          fields: ['_id']
+        });
+      }).then(function (res) {
+        res.docs.should.deep.equal([{_id: 'a'}]);
+        return db.find({
+          selector: {"foo.bar": 'yo', "bar.baz": 'sup'},
+          fields: ['_id']
+        });
+      }).then(function (res) {
+        res.docs.should.have.length(0);
+        return db.find({
+          selector: {"foo.bar": 'bruh', "bar.baz": 'nah'},
+          fields: ['_id']
+        });
+      }).then(function (res) {
+        res.docs.should.have.length(0);
+      });
+    });
+
+    it('should create a deep multi mapper, tricky docs', function () {
+      var db = context.db;
+      var index = {
+        "index": {
+          "fields": [
+            "foo.bar", "bar.baz"
+          ]
+        }
+      };
+      return db.createIndex(index).then(function () {
+        return db.bulkDocs([
+          {_id: 'a', foo: {bar: 'yo'}, bar: {baz: 'hey'}},
+          {_id: 'b', foo: {bar: 'sup'}, bar: {baz: 'dawg'}},
+          {_id: 'c', foo: true, bar: "yo"},
+          {_id: 'd', foo: null, bar: []}
+        ]);
+      }).then(function () {
+        return db.find({
+          selector: {"foo.bar": 'yo', "bar.baz": 'hey'},
+          fields: ['_id']
+        });
+      }).then(function (res) {
+        res.docs.should.deep.equal([{_id: 'a'}]);
+        return db.find({
+          selector: {"foo.bar": 'yo', "bar.baz": 'sup'},
+          fields: ['_id']
+        });
+      }).then(function (res) {
+        res.docs.should.have.length(0);
+        return db.find({
+          selector: {"foo.bar": 'bruh', "bar.baz": 'nah'},
+          fields: ['_id']
+        });
+      }).then(function (res) {
+        res.docs.should.have.length(0);
+      });
+    });
   });
 };
