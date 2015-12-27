f852af461ab62615aa3642ef2bfd2f30599c1a8b
Nolan Lawson <nolan.lawson@gmail.com>
2015-12-27 15:06:29 -0800

    fix node tests

--- tests/integration/utils.js
+++ tests/integration/utils.js
@@ -302,7 +302,7 @@ var testDir;
 if (typeof module !== 'undefined' && module.exports) {
   global.PouchDB = require('../../lib');
   global.binaryStringToBlobOrBuffer =
-    require('../../lib/deps/binary/binaryStringToBlobOrBuffer');
+    global.PouchDB.binaryStringToBlobOrBuffer;
   if (process.env.LEVEL_ADAPTER || process.env.LEVEL_PREFIX) {
     var defaults = {};
 
@@ -323,7 +323,7 @@ if (typeof module !== 'undefined' && module.exports) {
     testDir = process.env.TESTS_DIR ? process.env.TESTS_DIR : './tmp';
     testDir = testDir.slice(-1) === '/' ? testDir : testDir + '/';
     global.PouchDB.prefix = testDir + global.PouchDB.prefix;
-    require('../../lib/adapters/leveldb').use_prefix = true;
+    global.PouchDB.adapters.leveldb.use_prefix = true;
   }
   module.exports = testUtils;
 }
