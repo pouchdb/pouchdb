cf92fd2cb4c95ae10c128b665c8acb6f4cc5f156
Benoit Benezech <benoit.benezech@gmail.com>
2016-01-15 12:12:14 +0100

    processPendingBatch(false) is needed for non-continuous replications
    It may not be a big problem in practice, but theoretically, adding a
    change may require a batch cleanup since we have a 100 changes limit
    per batch.

--- src/replicate/replicate.js
+++ src/replicate/replicate.js
@@ -275,9 +275,7 @@ function replicate(src, target, opts, returnValue, result) {
     }
     pendingBatch.seq = change.seq;
     pendingBatch.changes.push(change);
-    if (changesOpts.live) {
-      processPendingBatch(true);
-    }
+    processPendingBatch(changesOpts.live);
   }
 
 
