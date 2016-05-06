cfd5839bff5d8391ccb3a0a55450519ccdb289e0
Nolan Lawson <nolan.lawson@gmail.com>
2016-05-05 19:56:57 -0700

    add levelalt adapters

---
+++ packages/pouchdb-adapter-fruitdown/src/index.js
@@ -0,0 +1,32 @@
+import CoreLevelPouch from 'pouchdb-adapter-leveldb-core';
+import { toPromise } from 'pouchdb-utils';
+import { extend as extend } from 'js-extend';
+
+import fruitdown from 'fruitdown';
+
+function FruitDownPouch(opts, callback) {
+  var _opts = extend({
+    db: fruitdown
+  }, opts);
+
+  CoreLevelPouch.call(this, _opts, callback);
+}
+
+// overrides for normal LevelDB behavior on Node
+FruitDownPouch.valid = function () {
+  return !!global.indexedDB;
+};
+FruitDownPouch.use_prefix = true;
+
+FruitDownPouch.destroy = toPromise(function (name, opts, callback) {
+  if (typeof opts === 'function') {
+    callback = opts;
+    opts = {};
+  }
+  var _opts = extend({
+    db: fruitdown
+  }, opts);
+
+  return CoreLevelPouch.destroy(name, _opts, callback);
+});
+export default FruitDownPouch;
---
+++ packages/pouchdb-adapter-memory/src/index.js
@@ -0,0 +1,32 @@
+import CoreLevelPouch from 'pouchdb-adapter-leveldb-core';
+import { toPromise } from 'pouchdb-utils';
+import { extend as extend } from 'js-extend';
+
+import memdown from 'memdown';
+
+function MemDownPouch(opts, callback) {
+  var _opts = extend({
+    db: memdown
+  }, opts);
+
+  CoreLevelPouch.call(this, _opts, callback);
+}
+
+// overrides for normal LevelDB behavior on Node
+MemDownPouch.valid = function () {
+  return true;
+};
+MemDownPouch.use_prefix = false;
+
+MemDownPouch.destroy = toPromise(function (name, opts, callback) {
+  if (typeof opts === 'function') {
+    callback = opts;
+    opts = {};
+  }
+  var _opts = extend({
+    db: memdown
+  }, opts);
+
+  return CoreLevelPouch.destroy(name, _opts, callback);
+});
+export default MemDownPouch;
