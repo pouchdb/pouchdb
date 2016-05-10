5a4a922bb498eaa28dab716c4377154f166156d1
Will Holley <willholley@gmail.com>
2016-05-10 16:39:19 +0100

    (#5140) - remove use of buffer.toArrayBuffer()
    buffer dropped support for toArrayBuffer
    (see https://github.com/feross/buffer/issues/90). However, all the
    browsers we support have typed array support so the storedObject will
    be implemented as a typed array from which we can directly construct
    a Blob.

--- src/adapters/leveldb/readAsBlobOrBuffer-browser.js
+++ src/adapters/leveldb/readAsBlobOrBuffer-browser.js
@@ -2,8 +2,10 @@ import createBlob from '../../deps/binary/blob';
 
 function readAsBlobOrBuffer(storedObject, type) {
   // In the browser, we've stored a binary string. This now comes back as a
-  // browserified Node-style Buffer, but we want a Blob instead.
-  return createBlob([storedObject.toArrayBuffer()], {type: type});
+  // browserified Node-style Buffer (implemented as a typed array),
+  // but we want a Blob instead.
+  var byteArray = new Uint8Array(storedObject);
+  return createBlob([byteArray], {type: type});
 }
 
 export default readAsBlobOrBuffer;
