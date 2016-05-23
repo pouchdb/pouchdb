0be04713d0b3d2cc13bbb6f1510e426f7dbf8a53
Nolan Lawson <nolan.lawson@gmail.com>
2016-05-22 17:45:23 -0700

    (#5203) - fix rebuild before `npm t`

--- bin/build-node.sh
+++ bin/build-node.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # don't bother doing this in travis because it's already been built
-if [ ! -z $TRAVIS ]; then
+if [ -z $TRAVIS ]; then
   BUILD_NODE=1 npm run build-modules
 fi
