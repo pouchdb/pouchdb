eb5b8f03759c3cc30133356e65d53b0da8e0ef8a
Nolan Lawson <nolan.lawson@gmail.com>
2016-05-25 08:51:57 -0400

    (#5204) - skip non-directories in packages/

--- bin/build-modules.js
+++ bin/build-modules.js
@@ -11,18 +11,24 @@ var Promise = lie;
 var denodeify = require('denodeify');
 var fs = require('fs');
 var readDir = denodeify(fs.readdir);
+var stat = denodeify(fs.stat);
 
 var buildModule = require('./build-module');
 var buildPouchDB = require('./build-pouchdb');
 
 readDir('./packages').then(function (packages) {
   return Promise.all(packages.map(function (pkg) {
-    console.log('Building ' + pkg + '...');
-    if (pkg === 'pouchdb') {
-      return buildPouchDB();
-    } else {
-      return buildModule(path.resolve('./packages', pkg));
-    }
+    return stat(path.resolve('packages', pkg)).then(function (stat) {
+      if (!stat.isDirectory()) { // skip e.g. 'npm-debug.log'
+        return;
+      }
+      console.log('Building ' + pkg + '...');
+      if (pkg === 'pouchdb') {
+        return buildPouchDB();
+      } else {
+        return buildModule(path.resolve('./packages', pkg));
+      }
+    });
   }));
 }).catch(function (err) {
   console.error('build error');
