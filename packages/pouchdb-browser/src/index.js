1ec6d995348927aa165710403809ad1f8e909826
Dale Harvey <dale@arandomurl.com>
2016-05-26 11:44:47 +0100

    (#5224) - Rename adapter-indexeddb to adapter-idb

--- packages/pouchdb-browser/src/index.js
+++ packages/pouchdb-browser/src/index.js
@@ -1,6 +1,6 @@
 import PouchDB from 'pouchdb-core';
 
-import IDBPouch from 'pouchdb-adapter-indexeddb';
+import IDBPouch from 'pouchdb-adapter-idb';
 import WebSqlPouch from 'pouchdb-adapter-websql';
 import HttpPouch from 'pouchdb-adapter-http';
 import mapreduce from 'pouchdb-mapreduce';
