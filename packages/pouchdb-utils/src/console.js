8c6979bb0b51499de42790476f70e27b57c5e1f9
Jan Schulte <hello@unexpected-co.de>
2016-05-29 23:29:30 +0200

    convert rest parameters to use arguments instead
    This avoids a problem with a dependency which cannot deal with rest
    params

--- packages/pouchdb-utils/src/console.js
+++ packages/pouchdb-utils/src/console.js
@@ -1,5 +1,6 @@
-function console(method, ...args) {
+function console(method) {
   if (console !== 'undefined' && method in console) {
+    var args = Array.prototype.slice.call(arguments, 1);
     console[method].apply(null, args);
   }
 }
