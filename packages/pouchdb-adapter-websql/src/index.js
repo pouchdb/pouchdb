b600bdd249946aa9eeabefbe868d9987c9240723
Nolan Lawson <nolan.lawson@gmail.com>
2016-06-07 06:58:54 -0700

    (#5293) - fix SQLite Plugin usage, add test

--- packages/pouchdb-adapter-websql/src/index.js
+++ packages/pouchdb-adapter-websql/src/index.js
@@ -2,20 +2,30 @@ import WebSqlPouchCore from 'pouchdb-adapter-websql-core';
 import { extend } from 'js-extend';
 import valid from './valid';
 
-function websql(optsOrName, version, description, size) {
-  if (typeof sqlitePlugin !== 'undefined') {
-    // The SQLite Plugin started deviating pretty heavily from the
-    // standard openDatabase() function, as they started adding more features.
-    // It's better to just use their "new" format and pass in a big ol'
-    // options object.
-    return sqlitePlugin.openDatabase(optsOrName);
-  }
+function createOpenDBFunction(opts) {
+  return function (name, version, description, size) {
+    if (typeof sqlitePlugin !== 'undefined') {
+      // The SQLite Plugin started deviating pretty heavily from the
+      // standard openDatabase() function, as they started adding more features.
+      // It's better to just use their "new" format and pass in a big ol'
+      // options object. Also there are many options here that may come from
+      // the PouchDB constructor, so we have to grab those.
+      var sqlitePluginOpts = extend({}, opts, {
+        name: name,
+        version: version,
+        description: description,
+        size: size
+      });
+      return sqlitePlugin.openDatabase(sqlitePluginOpts);
+    }
 
-  // Traditional WebSQL API
-  return openDatabase(optsOrName, version, description, size);
+    // Traditional WebSQL API
+    return openDatabase(name, version, description, size);
+  };
 }
 
 function WebSQLPouch(opts, callback) {
+  var websql = createOpenDBFunction(opts);
   var _opts = extend({
     websql: websql
   }, opts);
