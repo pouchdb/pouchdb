91ad4ede248edc816410e1c111abece6bde8efe6
Nolan Lawson <nolan.lawson@gmail.com>
2016-06-25 07:45:50 -0700

    (#5400) - set peerDependencies on pouchdb-core

--- bin/set-version.js
+++ bin/set-version.js
@@ -23,8 +23,15 @@ jsonFiles.forEach(function (jsonFile) {
   var json = JSON.parse(fs.readFileSync(jsonFile), 'utf-8');
   json.version = version;
   // update version of all inner dependencies
-  var depsList = [json.dependencies || {}, json.devDependencies || {}];
+  var depsList = [
+    json.dependencies,
+    json.devDependencies,
+    json.peerDependencies
+  ];
   depsList.forEach(function (deps) {
+    if (!deps) {
+      return;
+    }
     Object.keys(deps).forEach(function (key) {
       if (packages.indexOf(key) !== -1) {
         deps[key] = version;
