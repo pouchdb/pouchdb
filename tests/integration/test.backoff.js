3df66d79ba0ee17ec3511274dfdc0d261da1a2aa
seigel <github@paddlethis.com>
2016-06-28 22:06:30 -0600

    Clean up and use es5 only

--- tests/integration/test.backoff.js
+++ tests/integration/test.backoff.js
@@ -1,42 +1,48 @@
 'use strict';
 
 var adapters = [
-    ['local', 'http']
+  ['local', 'http']
 ];
 
 adapters.forEach(function (adapters) {
-    var suiteName = 'test.backoff.js-' + adapters[0] + '-' + adapters[1];
-    describe(suiteName, function () {
+  var suiteName = 'test.backoff.js-' + adapters[0] + '-' + adapters[1];
+  describe(suiteName, function () {
 
-        var dbs = {};
+    var dbs = {};
 
-        beforeEach(function (done) {
-            dbs.name = testUtils.adapterUrl(adapters[0], 'testdb');
-            dbs.remote = testUtils.adapterUrl(adapters[1], 'test_repl_remote');
-            testUtils.cleanup([dbs.name, dbs.remote], done);
-        });
-
-        after(function (done) {
-            testUtils.cleanup([dbs.name, dbs.remote], done);
-        });
-
-        it('backoff event listeners', function (done) {
-            var remote = new PouchDB(dbs.remote);
-            var db = new PouchDB(dbs.name);
-            const rep = db.sync(remote, {
-                live: true,
-                retry: true,
-                heartbeat: 1,
-                timeout: 1,
-                back_off_function: function () { return 1; }
-            });
-
-            setTimeout(function () {
-              var numAListeners = rep.pull.listeners("active").length;
-              numAListeners.should.be.below(3);
-              done();
-            }, 600);
-        });
-
+    beforeEach(function (done) {
+      dbs.name = testUtils.adapterUrl(adapters[0], 'testdb');
+      dbs.remote = testUtils.adapterUrl(adapters[1], 'test_repl_remote');
+      testUtils.cleanup([dbs.name, dbs.remote], done);
     });
+
+    after(function (done) {
+      testUtils.cleanup([dbs.name, dbs.remote], done);
+    });
+
+    it('backoff event listeners', function (done) {
+      var remote = new PouchDB(dbs.remote);
+      var db = new PouchDB(dbs.name);
+      var replication = db.sync(remote, {
+        live: true,
+        retry: true,
+        heartbeat: 1,
+        timeout: 1,
+        back_off_function: function () {
+          return 1;
+        }
+      });
+
+      replication.on('complete', function () {
+        done();
+      });
+
+
+      setTimeout(function () {
+        var numberOfActiveListeners = replication.pull.listeners("active").length;
+        numberOfActiveListeners.should.be.below(3);
+        replication.cancel();
+      }, 600);
+    });
+  });
 });
