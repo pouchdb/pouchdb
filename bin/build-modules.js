a0e171261a1c1e5e9315f59476eb2270dd6d9891
Nolan Lawson <nolan@nolanlawson.com>
2016-09-11 06:14:14 -0700

    (#5659) - fix jsnext:main target for pouchdb

--- bin/build-modules.js
+++ bin/build-modules.js
@@ -16,20 +16,28 @@ var stat = denodeify(fs.stat);
 var buildModule = require('./build-module');
 var buildPouchDB = require('./build-pouchdb');
 
+function buildPackage(pkg) {
+  return stat(path.resolve('packages/node_modules', pkg)).then(function (stat) {
+    if (!stat.isDirectory()) { // skip e.g. 'npm-debug.log'
+      return;
+    }
+    console.log('Building ' + pkg + '...');
+    if (pkg === 'pouchdb') {
+      return buildPouchDB();
+    } else {
+      return buildModule(path.resolve('./packages/node_modules', pkg));
+    }
+  });
+}
+
 readDir('packages/node_modules').then(function (packages) {
-  return Promise.all(packages.map(function (pkg) {
-    return stat(path.resolve('packages/node_modules', pkg)).then(function (stat) {
-      if (!stat.isDirectory()) { // skip e.g. 'npm-debug.log'
-        return;
-      }
-      console.log('Building ' + pkg + '...');
-      if (pkg === 'pouchdb') {
-        return buildPouchDB();
-      } else {
-        return buildModule(path.resolve('./packages/node_modules', pkg));
-      }
-    });
-  }));
+  // `pouchdb-for-coverage` has to be built last because it depends on
+  // `pouchdb`, which has a particular index.es.js file as its jsnext:main
+  return Promise.all(packages.filter(function (pkg) {
+    return pkg !== 'pouchdb-for-coverage';
+  }).map(buildPackage)).then(function () {
+    return buildPackage('pouchdb-for-coverage');
+  });
 }).catch(function (err) {
   console.error('build error');
   console.error(err.stack);
