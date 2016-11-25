704f5ab14ba92abc342c1248603882010757ff8f
Nolan Lawson <nolan.lawson@gmail.com>
2016-11-25 13:51:57 -0800

    (#5930) - use Firefox 46.0.1 in Travis, add FIREFOX_BIN

--- bin/test-browser.js
+++ bin/test-browser.js
@@ -17,6 +17,7 @@ var username = process.env.SAUCE_USERNAME;
 var accessKey = process.env.SAUCE_ACCESS_KEY;
 
 var SELENIUM_VERSION = process.env.SELENIUM_VERSION || '2.53.1';
+var FIREFOX_BIN = process.env.FIREFOX_BIN;
 
 // BAIL=0 to disable bailing
 var bail = process.env.BAIL !== '0';
@@ -187,6 +188,9 @@ function startTest() {
     'idle-timeout': 599,
     'tunnel-identifier': tunnelId
   };
+  if (FIREFOX_BIN) {
+    opts.firefox_binary = FIREFOX_BIN;
+  }
 
   sauceClient.init(opts).get(testUrl, function () {
 
