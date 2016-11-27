d996f328ffa089a277a4eed8abae51b636b2920a
Nolan Lawson <nolan.lawson@gmail.com>
2016-11-27 11:02:19 -0800

    (#5908 #5930) - cache CouchDB 1.6.1 Docker image on Travis

--- bin/run-couchdb-on-travis.sh
+++ bin/run-couchdb-on-travis.sh
@@ -7,7 +7,19 @@ if [ "$SERVER" = "couchdb-master" ]; then
   COUCH_PORT=3001
 else
   # Install CouchDB Stable
-  docker run -d -p 3000:5984 klaemo/couchdb:1.6.1
+  mkdir -p $HOME/docker
+  export DOCKER_CACHE_FILE=$HOME/docker/couchdb-1.6.1.tgz
+  if [ -f "$DOCKER_CACHE_FILE" ]; then
+    echo "Using cached Docker image for CouchDB 1.6.1..."
+    gunzip -c "$DOCKER_CACHE_FILE" | docker import - couchdb-1.6.1
+    docker run --name couchdb-1.6.1 -d -p 3000:5984
+  else
+    echo "Using uncached Docker image for CouchDB 1.6.1..."
+    # Cache CouchDB 1.6.1 Docker image on Travis; it won't ever change
+    # See https://giorgos.sealabs.net/docker-cache-on-travis-and-docker-112.html
+    docker run --name couchdb-1.6.1 -d -p 3000:5984 klaemo/couchdb:1.6.1
+    docker export couchdb-1.6.1 | gzip > "$DOCKER_CACHE_FILE"
+  fi
   COUCH_PORT=3000
 fi
 
