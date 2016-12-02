9abef3a162f9a117497aaca9c3273d20cf7bb23c
Nolan Lawson <nolan.lawson@gmail.com>
2016-12-02 10:53:49 +0100

    (#5962) - fix broken verify-bundle-size.sh script

--- bin/verify-bundle-size.sh
+++ bin/verify-bundle-size.sh
@@ -4,11 +4,16 @@
 # We might have to change this later.
 MAX=50000
 
-SIZE=`gzip -c packages/node_modules/pouchdb/dist/pouchdb.min.js | wc -c`
+# testing pouchdb.js instead of pouchdb.min.js because minification isn't run in Travis
+# in order to make our builds faster
+SIZE=`./node_modules/.bin/uglifyjs -mc < packages/node_modules/pouchdb/dist/pouchdb.js 2>/dev/null | gzip -c | wc -c`
 
-echo "Checking that pouchdb.min.js size $SIZE is less than $MAX"
+echo "Checking that pouchdb.min.js size $SIZE is less than $MAX and greater than 20"
 
-if [ "$SIZE" -lt "$MAX" ]; then
+if [ "$SIZE" -lt 21 ]; then
+  echo Failure
+  exit 1
+elif [ "$SIZE" -lt "$MAX" ]; then
   echo Success
   exit 0
 else
