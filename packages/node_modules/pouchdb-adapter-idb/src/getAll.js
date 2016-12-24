268116f18cfe1e32d3d22807f96f88198b3d250a
Nolan Lawson <nolan.lawson@gmail.com>
2016-12-23 18:21:05 -0800

    (#6059) - simpler implementation of batched allDocs()

---
+++ packages/node_modules/pouchdb-adapter-idb/src/getAll.js
@@ -0,0 +1,28 @@
+// simple shim for objectStore.getAll(), falling back to IDBCursor
+function getAll(objectStore, keyRange, onSuccess) {
+  if (typeof objectStore.getAll === 'function') {
+    // use native getAll
+    objectStore.getAll(keyRange).onsuccess = onSuccess;
+    return;
+  }
+  // fall back to cursors
+  var values = [];
+
+  function onCursor(e) {
+    var cursor = e.target.result;
+    if (cursor) {
+      values.push(cursor.value);
+      cursor.continue();
+    } else {
+      onSuccess({
+        target: {
+          result: values
+        }
+      });
+    }
+  }
+
+  objectStore.openCursor(keyRange).onsuccess = onCursor;
+}
+
+export default getAll;
