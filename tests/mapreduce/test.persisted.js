a72a60df999cd1c10a540cdc146b07fca249ef9c
Nolan Lawson <nolan.lawson@gmail.com>
2017-01-03 08:29:07 -0800

    (#6099) - use Map/Set in viewCleanup()

--- tests/mapreduce/test.persisted.js
+++ tests/mapreduce/test.persisted.js
@@ -460,6 +460,58 @@ function tests(suiteName, dbName, dbType) {
       });
     }
 
+    it('test docs with reserved IDs', function () {
+      var db = new PouchDB(dbName);
+
+      var docs = [
+        {_id: 'constructor'},
+        {_id: 'isPrototypeOf'},
+        {_id: 'hasOwnProperty'},
+        {
+          _id : '_design/view',
+          views : {
+            view : {
+              map : "function(doc){emit(doc._id);}"
+            }
+          }
+        }
+      ];
+      return db.bulkDocs(docs).then(function () {
+        return db.query('view/view', {include_docs: true});
+      }).then(function (res) {
+        var rows = res.rows.map(function (row) {
+          return {
+            id: row.id,
+            key: row.key,
+            docId: row.doc._id
+          };
+        });
+        assert.deepEqual(rows, [
+          { "id": "constructor",
+            "key": "constructor",
+            "docId": "constructor"
+          },
+          {
+            "id": "hasOwnProperty",
+            "key": "hasOwnProperty",
+            "docId": "hasOwnProperty"
+          },
+          {
+            "id": "isPrototypeOf",
+            "key": "isPrototypeOf",
+            "docId": "isPrototypeOf"
+          }
+        ]);
+        return db.viewCleanup();
+      }).then(function () {
+        return db.get('_design/view');
+      }).then(function (doc) {
+        return db.remove(doc);
+      }).then(function () {
+        return db.viewCleanup();
+      });
+    });
+
     it('should handle user errors in design doc names', function () {
       var db = new PouchDB(dbName);
       return db.put({
