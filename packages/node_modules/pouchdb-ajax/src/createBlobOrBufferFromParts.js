f7b1672445a2b8942f6751ea50a925fa8b1d7f44
Nolan Lawson <nolan.lawson@gmail.com>
2017-02-07 09:09:13 -0800

    build 6.1.2

--- packages/node_modules/pouchdb-ajax/src/createBlobOrBufferFromParts.js
+++
@@ -1,9 +0,0 @@
-import bufferFrom from 'buffer-from'; // ponyfill for Node <6
-
-function createBlobOrBufferFromParts(parts) {
-  return Buffer.concat(parts.map(function (part) {
-    return bufferFrom(part, 'binary');
-  }));
-}
-
-export default createBlobOrBufferFromParts;
