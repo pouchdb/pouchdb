5e16a997666bac2d4dfed4b349793e707b11fc81
jk47 <jhnkotz@gmail.com>
2017-02-20 12:21:11 -0600

    (#6234) moving to local area

--- tests/mapreduce/test.persisted.js
+++ tests/mapreduce/test.persisted.js
@@ -670,31 +670,6 @@ function tests(suiteName, dbName, dbType) {
       });
     });
 
-    it('should throw when user supplies map func unallowed by strict mode (#6234)', function () {
-      var db = new PouchDB(dbName);
-      var ddoc = {
-        _id: '_design/barbar',
-        views: {
-          scores: {
-            map: "function() { globalVar = 'hello'; }"
-          }
-        }
-      };
-      var doc = {
-        _id: 'foo',
-        firstName: 'Foobar',
-        lastName: 'Bazman'
-      };
-      return db.bulkDocs({docs: [ddoc, doc]}).then(function () {
-        return db.query('barbar/scores').then(function () {
-          throw new Error('expected an error in strict mode');
-        }, function (err) {
-          should.exist(err);
-        });
-      });
-    });
-
-
     var isNode = typeof window === 'undefined';
     if (dbType === 'local' && isNode) {
       it('#239 test memdown db', function () {
@@ -790,6 +765,30 @@ function tests(suiteName, dbName, dbType) {
             return chain;
           }).then(function () {
             PouchDB.removeAllListeners('destroyed');
+          });
+        });
+      });
+
+      it('should throw when user supplies map func unallowed by strict mode (#6234)', function () {
+        var db = new PouchDB(dbName);
+        var ddoc = {
+          _id: '_design/barbar',
+          views: {
+            scores: {
+              map: "function() { globalVar = 'hello'; }"
+            }
+          }
+        };
+        var doc = {
+          _id: 'foo',
+          firstName: 'Foobar',
+          lastName: 'Bazman'
+        };
+        return db.bulkDocs({docs: [ddoc, doc]}).then(function () {
+          return db.query('barbar/scores').then(function () {
+            throw new Error('expected an error in strict mode');
+          }, function (err) {
+            should.exist(err);
           });
         });
       });
