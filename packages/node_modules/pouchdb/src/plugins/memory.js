05ba7a9838ccedac27b731bacf2d3902d72bfafb
Nolan Lawson <nolan@nolanlawson.com>
2017-03-23 10:52:05 -0700

    (#6347) - fix build process, put pouchdb-find dist/ in pouchdb pkg

--- packages/node_modules/pouchdb/src/plugins/memory.js
+++ packages/node_modules/pouchdb/src/plugins/memory.js
@@ -1,13 +1,14 @@
 /* global PouchDB */
 
+// this code only runs in the browser, as its own dist/ script
+
 import MemoryPouchPlugin from 'pouchdb-adapter-memory';
 import { guardedConsole } from 'pouchdb-utils';
 
-var PDB = (typeof PouchDB !== 'undefined') ? PouchDB : require('pouchdb');
-if (!PDB) {
+if (typeof PouchDB === 'undefined') {
   guardedConsole('error', 'memory adapter plugin error: ' +
     'Cannot find global "PouchDB" object! ' +
     'Did you remember to include pouchdb.js?');
 } else {
-  MemoryPouchPlugin(PDB);
+  PouchDB.plugin(MemoryPouchPlugin);
 }
