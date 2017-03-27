aed9f01e8f499eb322e401a7ffb6cf9e8d5a17fa
Nolan Lawson <nolan.lawson@gmail.com>
2017-03-27 09:31:26 -0700

    (#6378) - remove scope-eval dep, use our own

---
+++ packages/node_modules/pouchdb-utils/src/scopeEval.js
@@ -0,0 +1,18 @@
+// Based on https://github.com/alexdavid/scope-eval v0.0.3
+// (source: https://unpkg.com/scope-eval@0.0.3/scope_eval.js)
+// This is basically just a wrapper around new Function()
+
+function scopeEval(source, scope) {
+  var keys = [];
+  var values = [];
+  for (var key in scope) {
+    if (scope.hasOwnProperty(key)) {
+      keys.push(key);
+      values.push(scope[key]);
+    }
+  }
+  keys.push(source);
+  return Function.apply(null, keys).apply(null, values);
+}
+
+export default scopeEval;
