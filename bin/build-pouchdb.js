09c184e182a95ea379ce2542441c12cda109c3f9
Nolan Lawson <nolan.lawson@gmail.com>
2017-03-28 18:06:38 -0700

    (#6386) - replace argsarray with custom impl

--- bin/build-pouchdb.js
+++ bin/build-pouchdb.js
@@ -18,7 +18,6 @@ var rollupPlugins = require('./rollupPlugins');
 var rimraf = denodeify(require('rimraf'));
 var mkdirp = denodeify(require('mkdirp'));
 var all = Promise.all.bind(Promise);
-var argsarray = require('argsarray');
 var buildUtils = require('./build-utils');
 var addPath = buildUtils.addPath;
 var doUglify = buildUtils.doUglify;
@@ -169,7 +168,8 @@ function buildPouchDBNext() {
   });
 }
 
-var rimrafMkdirp = argsarray(function (args) {
+var rimrafMkdirp = function () {
+  var args = Array.prototype.slice.apply(arguments);
   return all(args.map(function (otherPath) {
     return rimraf(addPath('pouchdb', otherPath));
   })).then(function () {
@@ -177,15 +177,16 @@ var rimrafMkdirp = argsarray(function (args) {
       return mkdirp(addPath('pouchdb', otherPath));
     }));
   });
-});
+};
 
-var doAll = argsarray(function (args) {
+var doAll = function () {
+  var args = Array.prototype.slice.apply(arguments);
   return function () {
     return all(args.map(function (promiseFactory) {
       return promiseFactory();
     }));
   };
-});
+};
 
 function doBuildNode() {
   return mkdirp(addPath('pouchdb', 'lib/plugins'))
