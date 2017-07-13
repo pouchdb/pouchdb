b0f8aee73b9a87540ca718d2cb87f223e57e6fc0
Will Holley <willholley@gmail.com>
2017-07-13 11:54:56 +0100

    build 6.3.0

--- packages/node_modules/pouchdb-ajax/src/defaultBody.js
+++
@@ -1,7 +0,0 @@
-import bufferFrom from 'buffer-from'; // ponyfill for Node <6
-
-function defaultBody() {
-  return bufferFrom('', 'binary');
-}
-
-export default defaultBody;
--- tests/find/test-suite-1/test.aaa-setup.js
+++
@@ -1,7 +0,0 @@
-if (typeof process === 'undefined' || process.browser) {
-  // to avoid having to bundle anything, we just use a global window.testCases
-// object and then run those
-  window.testCases = [];
-} else { // node
-  global.testCases = [];
-}
