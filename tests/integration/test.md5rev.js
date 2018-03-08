ba488b44ca73c8d422f120c249535b5da1921d24
Garren Smith <garren.smith@gmail.com>
2018-03-08 16:32:38 +0200

    add some extra tests

--- tests/integration/test.md5rev.js
+++ tests/integration/test.md5rev.js
@@ -51,5 +51,69 @@ adapters.forEach(function (adapter) {
       });
     });
 
+    it('includes revision in md5 hash', function () {
+      const doc = {
+        _id: '123-this-is-an-id',
+        hello: 'world',
+        'testing': 123,
+        '_rev': '1-63c3b22973694224bb406e470152b6e4'
+      };
+
+      const doc1 = Object.assign({}, doc);
+      doc1._rev = '1-99c3b22973694224bb406e470152aaaa';
+
+      var db1 = PouchDB(dbs.name1);
+      var db2 = PouchDB(dbs.name2);
+
+      return Promise.all([
+        db1.bulkDocs([doc], {new_edits: false}),
+        db2.bulkDocs([doc1], {new_edits: false})
+      ])
+      .then(function () {
+        doc.another = 'field';
+        doc1.another = 'field';
+
+        return Promise.all([db1.put(doc), db2.put(doc1)]);
+      })
+      .then(function (resp) {
+        resp[0].rev.should.not.equal(resp[1].rev);
+      });
+    });
+
+    it('replication and then update and delete creates same rev', function (done) {
+      const doc = {
+        _id: '123-this-is-an-id',
+        hello: 'world',
+        'testing': 123
+      };
+
+      var db1 = PouchDB(dbs.name1);
+      var db2 = PouchDB(dbs.name2);
+
+      db1.put(doc)
+      .then(function (resp) {
+        db2.replicate.from(db1)
+        .on('complete', function () {
+          doc._rev = resp.rev;
+          doc.newField = true;
+          return Promise.all([db1.put(doc), db2.put(doc)])
+          .then(function (resp) {
+            doc._rev = resp[0].rev;
+            return Promise.all([db1.remove(doc), db2.remove(doc)]);
+          })
+          .then(function (resp) {
+            return Promise.all([
+              db1.get(doc._id, {rev: resp[0].rev, revs_info: true}),
+              db2.get(doc._id, {rev: resp[0].rev, revs_info: true})
+            ]);
+          })
+          .then(function (resp) {
+            resp[0].should.deep.equal(resp[1]);
+            done();
+          });
+        });
+
+      });
+    });
   });
 });
