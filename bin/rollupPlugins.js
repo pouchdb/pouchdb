6fd65b453a9f095410270eac2e0f20a0cc229958
phil-lgr <pleger2@gmail.com>
2019-02-17 16:53:32 -0500

    (#7647) - Convert adapter.js in ES6 syntax (#7647) - Add babel config

--- bin/rollupPlugins.js
+++ bin/rollupPlugins.js
@@ -1,12 +1,17 @@
 'use strict';
 
 var nodeResolve = require('rollup-plugin-node-resolve');
+var babel = require('rollup-plugin-babel');
 var replace = require('rollup-plugin-replace');
 var inject = require('rollup-plugin-inject');
 
 function rollupPlugins(nodeResolveConfig) {
   return [
     nodeResolve(nodeResolveConfig),
+    babel({
+      exclude: 'node_modules/**',
+      plugins: ["@babel/plugin-proposal-class-properties"]
+    }),
     replace({
       // we have switches for coverage; don't ship this to consumers
       'process.env.COVERAGE': JSON.stringify(!!process.env.COVERAGE),
@@ -21,7 +26,7 @@ function rollupPlugins(nodeResolveConfig) {
       Map: ['pouchdb-collections', 'Map'],
       Set: ['pouchdb-collections', 'Set'],
       'Object.assign': ['pouchdb-utils', 'assign']
-    })
+    }),
   ];
 }
 
