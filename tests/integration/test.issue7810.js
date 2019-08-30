e75d18ff1b9b3b80c2d86cdd37eba3774d7c3e2a
pubkey <daniel_van_meyer@hotmail.de>
2019-08-30 19:30:57 +0200

    ADD test for #7810

---
+++ tests/integration/test.issue7810.js
@@ -0,0 +1,36 @@
+describe('#7810 query will not find documents when an index is used', function () {
+    beforeEach(function () {
+        return new PouchDB('mydb').destroy().then(function () {
+            return new PouchDB('mydb', { db: require('memdown') }).destroy();
+        });
+    });
+    var db = new PouchDB('mydb');
+    it('should find the documents even if a index is used', function () {
+        return db.createIndex({
+            index: {
+                fields: [
+                    'passportId'
+                ]
+            }
+        }).then(function () {
+            return db.put({
+                _id: 'foobar',
+                passportId: 'z3i7q29g4yr1',
+                firstName: 'Edison',
+                lastName: 'Keebler',
+                age: 24
+            });
+        }).then(function () {
+            return db.find({
+                selector: {
+                    _id: {}
+                },
+                limit: 1
+            });
+        }).then(function (docs) {
+            if (docs.docs.length !== 1) {
+                throw new Error('document not found');
+            }
+        });
+    });
+});
