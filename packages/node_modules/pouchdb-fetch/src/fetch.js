70ae5a241593b21e4b3158e8bb399156032caa07
Mario Casciaro <mariocasciaro@gmail.com>
2019-09-06 18:37:37 +0100

    (#7739) - Use proper AbortController implementation in pouchdb-fetch to be able to cancel long polling  requests

--- packages/node_modules/pouchdb-fetch/src/fetch.js
+++ packages/node_modules/pouchdb-fetch/src/fetch.js
@@ -2,13 +2,8 @@
 
 import nodeFetch, {Headers} from 'node-fetch';
 import fetchCookie from 'fetch-cookie';
+import AbortController from 'abort-controller';
 
 var fetch = fetchCookie(nodeFetch);
-
-/* We can fake the abort, the http adapter keeps track
-   of ignoring the result */
-function AbortController() {
-  return {abort: function () {}};
-}
 
 export {fetch, Headers, AbortController};
