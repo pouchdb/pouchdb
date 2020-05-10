4dcb0b162446809fbeb870bfa2935820a0820446
pubkey <8926560+pubkey@users.noreply.github.com>
2020-05-11 01:39:09 +0200

    IMPROVE test for #7810

--- tests/integration/test.issue7810.js
+++ tests/integration/test.issue7810.js
@@ -1,38 +1,63 @@
 describe('#7810 query will not find documents when an index is used', function () {
+    var dbNameWithIndex = 'db_with_index';
+    var dbNameWithoutIndex = 'db_without_index';
     beforeEach(function () {
-       return new PouchDB('mydb').destroy().then(function () {
-            return new PouchDB('mydb', { db: require('memdown') }).destroy();
-        });
+        return Promise.all(
+            [dbNameWithIndex, dbNameWithoutIndex].map(function (dbName) {
+                return new PouchDB(dbName).destroy().then(function () {
+                    return new PouchDB(dbName, { db: require('memdown') }).destroy();
+                });
+            })
+        );
     });
-    it('should find the documents even if a index is used', function () {
+    it('should find the same documents even if a index exists', function () {
         PouchDB.plugin(require('../../packages/node_modules/pouchdb-find'));
-        var CustomPouch = PouchDB.defaults({db: require('memdown')});
-        var db = new CustomPouch('mydb');
-        return db.createIndex({
+        var CustomPouch = PouchDB.defaults({ db: require('memdown') });
+
+        // create two databases
+        var dbWithIndex = new CustomPouch(dbNameWithIndex);
+        var dbWithoutIndex = new CustomPouch(dbNameWithoutIndex);
+
+        // add any index to one of the databases
+        return dbWithIndex.createIndex({
             index: {
                 fields: [
                     'passportId'
                 ]
             }
         }).then(function () {
-            return db.put({
+            // add the same document to both databases
+            var docData = {
                 _id: 'foobar',
                 passportId: 'z3i7q29g4yr1',
                 firstName: 'Edison',
                 lastName: 'Keebler',
                 age: 24
-            });
+            };
+            return Promise.all([
+                dbWithIndex.put(docData),
+                dbWithoutIndex.put(docData)
+            ]);
         }).then(function () {
-            return db.find({
+            // run the same query on both databases
+            var query = {
                 selector: {
                     _id: {}
                 },
                 limit: 1
-            });
-        }).then(function (docs) {
-            if (docs.docs.length !== 1) {
-                console.log(JSON.stringify(docs, null, 2));
-                throw new Error('document not found');
+            };
+            return Promise.all([
+                dbWithIndex.find(query),
+                dbWithoutIndex.find(query)
+            ]);
+        }).then(function (results) {
+            // both databases should have returned the same results
+            var resultWithIndex = results[0];
+            var resultWithoutIndex = results[1];
+
+            if (resultWithIndex.docs.length !== resultWithoutIndex.docs.length) {
+                console.log(JSON.stringify(results, null, 2));
+                throw new Error('results are not equal');
             }
         });
     });
