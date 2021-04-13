4253fbe8a822acb5628c70d10e3fc861c9de43e4
Diana Thayer <garbados@gmail.com>
2021-04-13 09:06:14 -0700

    fix: use done callback at end of partial tests

--- tests/find/test-suite-1/test.partial.js
+++ tests/find/test-suite-1/test.partial.js
@@ -2,7 +2,7 @@
 
 testCases.push(function (dbType, context) {
   describe(`${dbType}: test.partial.js`, function () {
-    beforeEach(function () {
+    beforeEach(function (done) {
       const write = context.db.bulkDocs({ docs: [
         { _id: 'a', type: 'x', hello: 'world' },
         { _id: 'a', type: 'y', hello: 'world' }
@@ -15,9 +15,9 @@ testCases.push(function (dbType, context) {
         ddoc: 'test-partial',
         name: 'type-x'
       });
-      Promise.all([write, index]);
+      Promise.all([write, index]).then(done);
     });
-    it('should apply the partial filter', function () {
+    it('should apply the partial filter', function (done) {
       context.db.find({
         selector: { hello: 'world' },
         use_index: ['_design/test-partial', 'type-x']
@@ -25,6 +25,7 @@ testCases.push(function (dbType, context) {
         result.should.deep.equals({
           docs: [{ _id: 'a', type: 'x', hello: 'world' }]
         });
+        done();
       });
     });
   });
