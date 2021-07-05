9c4fd59b4bbb1526d16f6905663eee6b18f502d5
James Coglan <james@neighbourhood.ie>
2021-07-05 16:47:35 +0100

    Extract pouchdb-server setup code
    Having this code inline in the if-statement that decides what do do
    based on the $SERVER variable makes this section harder to follow, so
    I've moved all the setup code into functions that are invoked where
    needed.

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -7,42 +7,56 @@
 : ${CLIENT:="node"}
 : ${COUCH_HOST:="http://127.0.0.1:5984"}
 
+pouchdb-setup-server() {
+  # in travis, link pouchdb-servers dependencies on pouchdb
+  # modules to the current implementations
+  mkdir pouchdb-server-install
+  cd pouchdb-server-install
+  npm init -y
+  npm install pouchdb-server
+  cd ..
+
+  for pkg in packages/node_modules/* ; do
+    pouchdb-link-server-modules "$(basename "$pkg")"
+  done
+
+  TESTDIR=./tests/pouchdb_server
+  rm -rf $TESTDIR && mkdir -p $TESTDIR
+  FLAGS="$POUCHDB_SERVER_FLAGS --dir $TESTDIR"
+  echo -e "Starting up pouchdb-server with flags: $FLAGS \n"
+  ./pouchdb-server-install/node_modules/.bin/pouchdb-server -n -p 6984 $FLAGS &
+  export SERVER_PID=$!
+}
+
+pouchdb-link-server-modules() {
+  local pkg="$1"
+
+  cd "packages/node_modules/${pkg}"
+  npm link
+  cd ../../../pouchdb-server-install/
+
+  # node_modules of pouchdb-server-install
+  if [ -d "node_modules/${pkg}" ]; then
+    echo -e "\nnpm link ${pkg} for pouchdb-server-install"
+    npm link "${pkg}"
+  fi
+
+  # internal node_modules of other packages
+  for subPkg in $(ls -d node_modules/**/node_modules/${pkg}/ 2>/dev/null); do
+    cd ${subPkg}../..
+    echo -e "\nnpm link ${pkg} for ${subPkg}"
+    npm link "${pkg}"
+    cd ../..
+  done
+
+  cd ..
+}
+
 if [[ ! -z $SERVER ]]; then
   if [ "$SERVER" == "pouchdb-server" ]; then
     export COUCH_HOST='http://127.0.0.1:6984'
     if [[ "$TRAVIS_REPO_SLUG" == "pouchdb/pouchdb" || "$COVERAGE" == 1 ]]; then
-      # in travis, link pouchdb-servers dependencies on pouchdb
-      # modules to the current implementations
-      mkdir pouchdb-server-install
-      cd pouchdb-server-install
-      npm init -y
-      npm install pouchdb-server
-      cd ..
-      PKGS=$(ls packages/node_modules);
-      for pkg in $PKGS; do
-        cd packages/node_modules/${pkg}
-        npm link
-        cd ../../../pouchdb-server-install/
-        # node_modules of pouchdb-server-install
-        if [ -d "node_modules/${pkg}" ]; then
-          echo -e "\nnpm link ${pkg} for pouchdb-server-install"
-        npm link ${pkg}
-        fi
-        # internal node_modules of other packages
-        for subPkg in $(ls -d node_modules/**/node_modules/${pkg}/ 2>/dev/null); do
-          cd ${subPkg}../..
-          echo -e "\nnpm link ${pkg} for ${subPkg}"
-        npm link ${pkg}
-          cd ../..
-        done
-        cd ..
-      done
-      TESTDIR=./tests/pouchdb_server
-      rm -rf $TESTDIR && mkdir -p $TESTDIR
-      FLAGS="$POUCHDB_SERVER_FLAGS --dir $TESTDIR"
-      echo -e "Starting up pouchdb-server with flags: $FLAGS \n"
-      ./pouchdb-server-install/node_modules/.bin/pouchdb-server -n -p 6984 $FLAGS &
-      export SERVER_PID=$!
+      pouchdb-setup-server
     else
       echo -e "pouchdb-server should be running on $COUCH_HOST\n"
     fi
