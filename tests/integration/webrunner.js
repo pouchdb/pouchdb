40d0cdec53665cc26d7deee2cce3cb5c1270a94a
James Coglan <james@neighbourhood.ie>
2021-09-16 17:07:37 +0100

    Remove unneeded function wrapping in webrunner.js

--- tests/integration/webrunner.js
+++ tests/integration/webrunner.js
@@ -5,95 +5,90 @@
   var params = testUtils.params();
   var remote = params.remote === '1';
 
-  function loadScripts() {
+  function startTests() {
+    window.removeEventListener("load", startTests);
 
-    function startTests() {
-      window.removeEventListener("load", startTests);
+    if (remote) {
+      // Capture logs for selenium output
+      var logs = [];
 
-      if (remote) {
-        // Capture logs for selenium output
-        var logs = [];
+      (function () {
 
-        (function () {
-
-          function serializeLogItem(obj, filter, space) {
-            if (typeof obj === 'string') {
-              return obj;
-            } else if (obj instanceof Error) {
-              return obj.stack;
-            } else {
-              return JSON.stringify(obj, filter, space);
-            }
+        function serializeLogItem(obj, filter, space) {
+          if (typeof obj === 'string') {
+            return obj;
+          } else if (obj instanceof Error) {
+            return obj.stack;
+          } else {
+            return JSON.stringify(obj, filter, space);
           }
+        }
 
-          function wrappedLog(oldLog, type) {
-            return function () {
-              var args = Array.prototype.slice.call(arguments);
-              logs.push({
-                type: type,
-                content: args.map(function (arg) {
-                  return serializeLogItem(arg);
-                }).join(' ')
-              });
-              oldLog.apply(console, arguments);
-            };
-          }
-
-          console.log = wrappedLog(console.log, 'log');
-          console.error = wrappedLog(console.error, 'error');
-
-        })();
-
-        // Capture test events for selenium output
-        var testEventsBuffer = [];
-
-        window.testEvents = function () {
-          var events = testEventsBuffer;
-          testEventsBuffer = [];
-          return events;
-        };
-
-        mocha.reporter(function (runner) {
-          var eventNames = ['start', 'end', 'suite', 'suite end', 'pass', 'pending', 'fail'];
-          eventNames.forEach(function (name) {
-            runner.on(name, function (obj, err) {
-              testEventsBuffer.push({
-                name: name,
-                obj: obj && {
-                  root: obj.root,
-                  title: obj.title,
-                  duration: obj.duration,
-                  slow: typeof obj.slow === 'function' ? obj.slow() : undefined,
-                  fullTitle: typeof obj.fullTitle === 'function' ? obj.fullTitle() : undefined
-                },
-                err: err && {
-                  actual: err.actual,
-                  expected: err.expected,
-                  showDiff: err.showDiff,
-                  message: err.message,
-                  stack: err.stack,
-                  uncaught: err.uncaught
-                },
-                logs: logs
-              });
-              logs = [];
+        function wrappedLog(oldLog, type) {
+          return function () {
+            var args = Array.prototype.slice.call(arguments);
+            logs.push({
+              type: type,
+              content: args.map(function (arg) {
+                return serializeLogItem(arg);
+              }).join(' ')
             });
+            oldLog.apply(console, arguments);
+          };
+        }
+
+        console.log = wrappedLog(console.log, 'log');
+        console.error = wrappedLog(console.error, 'error');
+
+      })();
+
+      // Capture test events for selenium output
+      var testEventsBuffer = [];
+
+      window.testEvents = function () {
+        var events = testEventsBuffer;
+        testEventsBuffer = [];
+        return events;
+      };
+
+      mocha.reporter(function (runner) {
+        var eventNames = ['start', 'end', 'suite', 'suite end', 'pass', 'pending', 'fail'];
+        eventNames.forEach(function (name) {
+          runner.on(name, function (obj, err) {
+            testEventsBuffer.push({
+              name: name,
+              obj: obj && {
+                root: obj.root,
+                title: obj.title,
+                duration: obj.duration,
+                slow: typeof obj.slow === 'function' ? obj.slow() : undefined,
+                fullTitle: typeof obj.fullTitle === 'function' ? obj.fullTitle() : undefined
+              },
+              err: err && {
+                actual: err.actual,
+                expected: err.expected,
+                showDiff: err.showDiff,
+                message: err.message,
+                stack: err.stack,
+                uncaught: err.uncaught
+              },
+              logs: logs
+            });
+            logs = [];
           });
         });
-      }
-
-      mocha.run();
+      });
     }
 
-    testUtils.loadPouchDB().then(function () {
-      if (document.readyState === 'complete') {
-        startTests();
-      } else {
-        window.addEventListener("load", startTests);
-      }
-    });
+    mocha.run();
   }
 
-  loadScripts();
+  testUtils.loadPouchDB().then(function () {
+    if (document.readyState === 'complete') {
+      startTests();
+    } else {
+      window.addEventListener("load", startTests);
+    }
+  });
 
 })();
