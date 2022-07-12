5f61b1fe128bb6091d28555ee8d44f185371923f
Christopher Astfalk <christopher.astfalk@icloud.com>
2022-07-13 00:49:57 +0200

    fix partial filter find on indexeddb adapter

--- tests/find/test-suite-1/test.partial.js
+++ tests/find/test-suite-1/test.partial.js
@@ -17,28 +17,26 @@ describe('test.partial.js', function () {
     return Promise.all([write, index]);
   });
 
-  it('should write the partial filter to the ddoc', function () {
-    return context.db.get('_design/test-partial').then(function (result) {
-      const map = result.views['type-x'].map;
-      map.should.have.property('partial_filter_selector');
-      map.partial_filter_selector.should.deep.equal({
-        type: {
-          '$eq': 'x'
-        }
-      });
+  it('should write the partial filter to the ddoc', async function () {
+    const result = await context.db.get('_design/test-partial');
+    const map = result.views['type-x'].map;
+    map.should.have.property('partial_filter_selector');
+    map.partial_filter_selector.should.deep.equal({
+      type: {
+        '$eq': 'x'
+      }
     });
   });
 
-  it('should apply the partial filter', function () {
-    return context.db.find({
+  it('should apply the partial filter', async function () {
+    const result = await context.db.find({
       selector: { hello: 'world' },
       use_index: ['_design/test-partial', 'type-x']
-    }).then(function (result) {
-      result.docs.should.have.length(1);
-      const [{ _id: id, type, hello }] = result.docs;
-      id.should.equal('a');
-      type.should.equal('x');
-      hello.should.equal('world');
     });
+    result.docs.should.have.length(1);
+    const [{ _id: id, type, hello }] = result.docs;
+    id.should.equal('a');
+    type.should.equal('x');
+    hello.should.equal('world');
   });
 });
