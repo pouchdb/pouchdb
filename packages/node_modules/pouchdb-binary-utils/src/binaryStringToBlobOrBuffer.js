c750b014ff343ab4b4739f8e9bfbca56987b466e
Alba Herrerías <albaherreriasdev@gmail.com>
2022-11-11 14:42:51 +0000

    build 7.3.1

--- packages/node_modules/pouchdb-binary-utils/src/binaryStringToBlobOrBuffer.js
+++
@@ -1,7 +0,0 @@
-import typedBuffer from './typedBuffer';
-
-function binStringToBluffer(binString, type) {
-  return typedBuffer(binString, 'binary', type);
-}
-
-export default binStringToBluffer;
--- packages/node_modules/pouchdb-utils/src/flatten.js
+++
@@ -1,9 +0,0 @@
-function flatten(arrs) {
-  var res = [];
-  for (var i = 0, len = arrs.length; i < len; i++) {
-    res = res.concat(arrs[i]);
-  }
-  return res;
-}
-
-export default flatten;
