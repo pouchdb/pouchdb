40ac7a2639223845d04ca29f15d1525d4443f35c
Jan Lehnardt <jan@apache.org>
2022-12-16 11:11:37 +0100

    feat(site): faster website uploads, rsync skips files that are already on the server

--- bin/publish-site.sh
+++ bin/publish-site.sh
@@ -5,4 +5,4 @@ BUILD=1 npm run build-site
 
 # Push the site live, requires credentials, open a bug
 # if you need to be able to push the site
-scp -r docs/_site/* pouchdb@pouchdb.com:/home/pouchdb/www/pouchdb.com/
+rsync --recursive --progress docs/_site/* pouchdb@pouchdb.com:/home/pouchdb/www/pouchdb.com/
