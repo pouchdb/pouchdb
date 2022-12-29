6a7fd466105daacd7205dd6efe8075831558460b
vorotamoroz <vrtmrz@gmail.com>
2022-12-29 23:09:29 +0900

    (pouchdb#8581) - Fix test.

--- tests/integration/test.issue8581.js
+++ tests/integration/test.issue8581.js
@@ -1,30 +1,38 @@
-describe('test.issue8581.js', function () {
-  let iframe;
-  let callbackFnName = "";
-  afterEach(() => {
-    if (iframe) {
-      document.body.removeChild(iframe);
-    }
-    iframe = null;
-    delete window[callbackFnName];
-  });
+'use strict';
 
-  it('Should not raise error', async function () {
-    callbackFnName = "callback" + Date.now();
-    const okPromise = new Promise((res) => {
-      window[callbackFnName] = () => {
-        delete window[callbackFnName];
-        res();
-      };
+if (typeof window !== "undefined" && typeof window.document !== 'undefined') {
+  runTests();
+}
+
+function runTests() {
+  describe('test.issue8581.js', function () {
+    let iframe;
+    let callbackFnName = "";
+    afterEach(() => {
+      if (iframe) {
+        window.document.body.removeChild(iframe);
+      }
+      iframe = null;
+      delete window[callbackFnName];
     });
-    iframe = document.createElement("iframe");
-    iframe.src = "about:blank";
-    document.body.appendChild(iframe);
-    const target = iframe.contentDocument;
-    const scriptElement = target.createElement("script");
-    scriptElement.innerHTML = `localStorage.setItem("Hello",new Date());
+
+    it('#8581 Should not raise error', async function () {
+      callbackFnName = "callback" + Date.now();
+      const okPromise = new Promise((res) => {
+        window[callbackFnName] = () => {
+          delete window[callbackFnName];
+          res();
+        };
+      });
+      iframe = window.document.createElement("iframe");
+      iframe.src = "about:blank";
+      window.document.body.appendChild(iframe);
+      const target = iframe.contentDocument;
+      const scriptElement = target.createElement("script");
+      scriptElement.innerHTML = `localStorage.setItem("Hello",new Date());
 requestIdleCallback(()=>window.parent.${callbackFnName}(),{timeout:100});`;
-    target.body.appendChild(scriptElement);
-    await Promise.race([okPromise, new Promise(res => setTimeout(res, 250))]);
+      target.body.appendChild(scriptElement);
+      await Promise.race([okPromise, new Promise(res => setTimeout(res, 250))]);
+    });
   });
-});
+}
