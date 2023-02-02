9c282ab3a0c5895d5a017243f5ddd0eb4d3cf1da
vorotamoroz <vrtmrz@gmail.com>
2023-02-02 10:02:26 +0900

    (pouchdb#8581) - Fix test Fix format issues.

--- tests/integration/test.issue8581.js
+++ tests/integration/test.issue8581.js
@@ -21,18 +21,27 @@ function runTests() {
       callbackFnName = "callback" + Date.now();
       iframe = window.document.createElement("iframe", { src: "about:blank" });
       window.document.body.appendChild(iframe);
-    })
+    });
 
     it('#8581 Should not raise error', function (done) {
       window[callbackFnName] = (err) => {
-        if (timeout) timeout = clearTimeout(timeout);
+        if (timeout) {
+          timeout = clearTimeout(timeout);
+        }
         done(err);
         window[callbackFnName] = () => { };
       };
-      let timeout = setTimeout(() => callbackFnName in window && window[callbackFnName](new Error(`Timed out`)), 500);
+      let timeout = setTimeout(
+        () => {
+          if (callbackFnName in window) {
+            window[callbackFnName](new Error(`Timed out`));
+          }
+        }
+        , 500);
       const target = iframe.contentDocument;
       const scriptElement = target.createElement("script");
-      scriptElement.innerHTML = `localStorage.setItem("Hello",new Date());setTimeout(window.parent.${callbackFnName},100);`;
+      scriptElement.innerHTML =
+        `localStorage.setItem("Hello",new Date());setTimeout(window.parent.${callbackFnName},100);`;
       target.body.appendChild(scriptElement);
     });
   });
