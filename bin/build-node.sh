fde45b93111b218f473d16c2161397741faf4a15
Alba Herrerías <albaherreriasdev@gmail.com>
2023-02-09 11:09:47 +0000

    build 8.0.1

--- bin/build-node.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# don't bother doing this in GHA because it's already been built
-if [ -z $GITHUB_REPOSITORY ]; then
-  BUILD_NODE=1 npm run build-modules
-fi
--- bin/verify-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Verify various aspects of the build to make sure everything was
-# built correctly.
-
-npm run build
-
-node ./bin/verify-dependencies.js
-sh ./bin/verify-bundle-size.sh
