b75429c6c2e360c63d67dfc32f4e04fa238fc9f7
Frank Lemanschik <frank@lemanschik.com>
2023-06-14 07:27:56 +0000

    14. Juni 2023, 09:27

--- packages/pouchdb-binary-utils/src/blobOrBufferToBinaryString.js
+++ packages/pouchdb-binary-utils/src/blobOrBufferToBinaryString.js
@@ -1,9 +1,6 @@
-// not used in Node, but here for completeness
-function bufferToBinaryString(arrayBuffer) {
-  return String.fromCharCode(...new Uint8Array(arrayBuffer));
-}
-
-const toBase64 = (arrayBuffer) => btoa(bufferToBinaryString(arrayBuffer));
+const toBase64 = (arrayBuffer) => btoa(String.fromCharCode(
+  ...new Uint8Array(arrayBuffer)
+));
 
 function blobToBase64(blobOrBuffer, callback) {
   new Response(blobOrBuffer).arrayBuffer().then(toBase64).then(
