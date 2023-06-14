1aaa9847e13ca8ad6e195f03d26d422540e20140
Frank Lemanschik <frank@lemanschik.com>
2023-06-14 05:58:38 +0000

    14. Juni 2023, 07:58

--- bin/build.js
+++ bin/build.js
@@ -4,7 +4,9 @@ const nodeResolve = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
 const json = require('@rollup/plugin-json');
 const alias = require('@rollup/plugin-alias');
-
+const eslint = require('@rollup/plugin-eslint')({
+  fix:true,
+});
 const customResolver = nodeResolve({
   extensions: ['.mjs', '.js', '.jsx', '.json', '.sass', '.scss'],
 });
@@ -21,12 +23,12 @@ fs.readdirSync('packages').map(pkg=>[rollup.rollup({
     alias({
       customResolver, entries,
     }),
-    nodeResolve({preferBuiltins: true}),json(),commonjs()
+    nodeResolve({preferBuiltins: true}),json(),commonjs(),eslint
   ],
 }).then(bundle=>bundle.write({ dir: 'lib' })),rollup.rollup({ 
   input: { [pkg+'.browser.es']: pkg },
   plugins: [alias({
       customResolver, entries,
-    }),nodeResolve({preferBuiltins:false,browser:true}),json(),commonjs()
+    }),nodeResolve({preferBuiltins:false,browser:true}),json(),commonjs(),eslint
   ],
 }).then(bundle=>bundle.write({ dir: 'lib' }))]);
