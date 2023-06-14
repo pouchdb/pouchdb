321c8f6c707df4bb5a75a46debc7132bcd5a5b50
Frank Lemanschik <frank@lemanschik.com>
2023-06-14 08:06:53 +0000

    14. Juni 2023, 10:06

--- packages/pouchdb-crypto/index.js
+++ packages/pouchdb-crypto/index.js
@@ -5,19 +5,21 @@ export async function digestFromMessage(message,algo='SHA-256') {
     return { 
         digist(format='hex') {
             const formats = {
-                hex: () => Array.from(new Uint8Array(arrayBuffer)) // convert buffer to byte array
+                hex: () => 
+                Array.from(new Uint8Array(arrayBuffer))
+                // converted buffer to byte array
                 .map((b) => b.toString(16).padStart(2, "0"))
-                .join(""), // convert bytes to hex string;
+                .join(""), // converted bytes to hex string;
             }
-            
+            // Fails by design with wrong format            
             return formats[format]();
         } 
     }
 }
-
+// Old But gold
 const toBase64 = (arrayBuffer) => btoa(String.fromCharCode(
-    ...new Uint8Array(arrayBuffer)
-  ));
+  ...new Uint8Array(arrayBuffer)
+));
   
 function blobToBase64(blobOrBuffer, callback) {
    new Response(blobOrBuffer).arrayBuffer().then(toBase64).then(
