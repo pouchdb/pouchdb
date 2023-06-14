d760c69506c1b8d3d32bdd0641f859eff670196d
Frank Lemanschik <frank@lemanschik.com>
2023-06-14 07:59:30 +0000

    14. Juni 2023, 09:59

--- packages/pouchdb-crypto/index.js
+++ packages/pouchdb-crypto/index.js
@@ -1,4 +1,4 @@
-async function digestFromMessage(message,algo='SHA-256') {
+export async function digestFromMessage(message,algo='SHA-256') {
     const msgUint8 = new TextEncoder().encode(message);
     const hashBuffer = await crypto.subtle.digest(algo, msgUint8); // hash the message
     const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
@@ -6,4 +6,14 @@ async function digestFromMessage(message,algo='SHA-256') {
       .map((b) => b.toString(16).padStart(2, "0"))
       .join(""); // convert bytes to hex string
     return hashHex;
-  }
+}
+
+const toBase64 = (arrayBuffer) => btoa(String.fromCharCode(
+    ...new Uint8Array(arrayBuffer)
+  ));
+  
+function blobToBase64(blobOrBuffer, callback) {
+   new Response(blobOrBuffer).arrayBuffer().then(toBase64).then(
+   (b64)=>callback(null,b64),err=>callback(err));
+   //callback(blobOrBuffer.toString('binary'));
+}
