50db89aba28f770ecf273e8cdb7421e782c81a19
Frank Lemanschik <frank@lemanschik.com>
2023-06-15 11:40:23 +0000

    15. Juni 2023, 13:40

--- packages/pouchdb-adapter-utils/src/allDocsKeysQuery.js
+++ packages/pouchdb-adapter-utils/src/allDocsKeysQuery.js
@@ -3,14 +3,11 @@ export async function allDocsKeysQuery(api, opts) {
   const finalResults = {
     offset: opts.skip,
     rows: Promise.all(opts.keys.map(async (key) => {
-      const filterOpts = ({limit,skip,keys,...rest}) => Object.assign({key: key, deleted: 'ok'}, rest);
-      var subOpts = ;
-      
-      delete subOpts['limit'];
-      delete subOpts['skip'];
-      delete subOpts['keys'];
-      
-      api._allDocs(subOpts, function (err, res) {
+      const filterOpts = ({limit,skip,keys,...rest}) => 
+        Object.assign({key: key, deleted: 'ok'}, rest);
+      const subOpts = ;
+            
+      api._allDocs(filterOpts(opts), function (err, res) {
         /* istanbul ignore if */
         if (err) {
           throw new Error(err);
