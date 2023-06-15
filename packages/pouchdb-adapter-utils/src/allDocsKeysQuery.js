4bacc3d22c47f2070073059acd43e325a1ffc80d
Frank Lemanschik <frank@lemanschik.com>
2023-06-15 11:27:44 +0000

    15. Juni 2023, 13:27

--- packages/pouchdb-adapter-utils/src/allDocsKeysQuery.js
+++ packages/pouchdb-adapter-utils/src/allDocsKeysQuery.js
@@ -1,29 +1,30 @@
 export async function allDocsKeysQuery(api, opts) {
   var keys = opts.keys;
   var finalResults = {
-    offset: opts.skip
+    offset: opts.skip,
+    rows: keys.map((key) => async () => {
+      var subOpts = Object.assign({key: key, deleted: 'ok'}, opts);
+      ['limit', 'skip', 'keys'].forEach(function (optKey) {
+        delete subOpts[optKey];
+      });
+      await new Promise(function (resolve, reject) {
+        api._allDocs(subOpts, function (err, res) {
+          /* istanbul ignore if */
+          if (err) {
+            return reject(err);
+          }
+          /* istanbul ignore if */
+          if (opts.update_seq && res.update_seq !== undefined) {
+            finalResults.update_seq = res.update_seq;
+          }
+          finalResults.total_rows = res.total_rows;
+          resolve(res.rows[0] || {key: key, error: 'not_found'});
+        });
+      });
+    }),
   };
   
-  keys.map((key) => async () => {
-    var subOpts = Object.assign({key: key, deleted: 'ok'}, opts);
-    ['limit', 'skip', 'keys'].forEach(function (optKey) {
-      delete subOpts[optKey];
-    });
-    await new Promise(function (resolve, reject) {
-      api._allDocs(subOpts, function (err, res) {
-        /* istanbul ignore if */
-        if (err) {
-          return reject(err);
-        }
-        /* istanbul ignore if */
-        if (opts.update_seq && res.update_seq !== undefined) {
-          finalResults.update_seq = res.update_seq;
-        }
-        finalResults.total_rows = res.total_rows;
-        resolve(res.rows[0] || {key: key, error: 'not_found'});
-      });
-    });
-  })
+  
   finalResults.rows = results;
   return finalResults;
   
