8058d6f5a994f58d91c2e504baafd9369e5f8818
Frank Lemanschik <frank@lemanschik.com>
2023-06-15 05:55:54 +0000

    15. Juni 2023, 07:55

--- packages/pouchdb-core/src/index.js
+++ packages/pouchdb-core/src/index.js
@@ -1,10 +1,7 @@
-import PouchDB from './setup';
-import version from './version';
+import PouchDB from './setup.js';
 import pouchChangesFilter from 'pouchdb-changes-filter';
 
 // TODO: remove from pouchdb-core (breaking)
 PouchDB.plugin(pouchChangesFilter);
-
-PouchDB.version = version;
 
 export default PouchDB;
