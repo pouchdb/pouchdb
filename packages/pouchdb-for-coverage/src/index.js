983bbf70bc14204e83d4b87fbebb7d3ddf2df092
Frank Lemanschik <frank@lemanschik.com>
2023-06-15 13:04:07 +0000

    15. Juni 2023, 15:04

--- packages/pouchdb-for-coverage/src/index.js
+++ packages/pouchdb-for-coverage/src/index.js
@@ -3,7 +3,7 @@ import utils from './utils.js';
 import errors from './errors.js';
 import * as collate from 'pouchdb-lib/lib/pouchdb-collate.js';
 // explicitly include pouchdb-find so coverage captures it correctly
-import find from 'pouchdb-find';
+import find from 'pouchdb-lib/lib/pouchdb-find.js';
 
 PouchDB.utils = utils;
 PouchDB.Errors = errors;
