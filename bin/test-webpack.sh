bd59f92cbb5fc68cf600e49eb2f0a468171fd41a
alxndrsn <alxndrsn>
2023-06-20 16:14:35 +0000

    test-webpack: don't save new dependencies
    This removes the `-D` flag from `npm install`, which was introduced in
    404346bba500c357c0276ad15762e985fb143eea

--- bin/test-webpack.sh
+++ bin/test-webpack.sh
@@ -7,7 +7,7 @@
 #
 
 npm run build
-npm i webpack@5.66.0 -D webpack-cli@4.9.2 # do this on-demand to avoid slow installs
+npm i webpack@5.66.0 webpack-cli@4.9.2 # do this on-demand to avoid slow installs
 node bin/update-package-json-for-publish.js
 ./node_modules/.bin/webpack
 BUILD_NODE_DONE=0 POUCHDB_SRC='../../pouchdb-webpack.js' npm test
