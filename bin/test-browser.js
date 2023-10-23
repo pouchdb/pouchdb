ea6090ab62bf03ff17b5ffc68f02e8d39ea83855
alxndrsn <alxndrsn>
2023-10-23 05:57:22 +0000

    Update comment

--- bin/test-browser.js
+++ bin/test-browser.js
@@ -163,9 +163,9 @@ async function startTest() {
   page.on('pageerror', err => {
     if (browserName === 'webkit' && err.toString()
         .match(/^Fetch API cannot load http.* due to access control checks.$/)) {
-      // This is an _uncatchable_, nonsensical error seen in playwright v1.36.1
-      // webkit.  If it is ignored, fetch() will also throw a _catchable_
-      // TypeError: Load failed
+      // This is an _uncatchable_, error seen in playwright v1.36.1 webkit. If
+      // it is ignored, fetch() will also throw a _catchable_:
+      // `TypeError: Load failed`
       console.log('Ignoring error:', err);
       return;
     }
