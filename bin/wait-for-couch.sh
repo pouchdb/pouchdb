15819148c93f0237b06c50a79222c202a900d403
alxndrsn <alxndrsn>
2024-03-06 12:24:03 +0000

    shellcheck

--- bin/wait-for-couch.sh
+++ bin/wait-for-couch.sh
@@ -5,9 +5,9 @@ maxWait="${1:-0}"
 printf "Waiting for host to start on %s..." "$COUCH_HOST"
 
 WAITING=0
-until [ '200' = "$(curl -s -o /dev/null -w '%{http_code}' ${COUCH_HOST})" ]; do
+until [[ '200' = "$(curl -s -o /dev/null -w '%{http_code}' "$COUCH_HOST")" ]]; do
   ((WAITING=WAITING+1))
-  if [ $WAITING -eq $maxWait ]; then
+  if [ $WAITING -eq "$maxWait" ]; then
     printf '\nHost failed to start\n'
     exit 1
   fi
