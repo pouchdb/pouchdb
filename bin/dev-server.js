668520327ab1de81aa731498c5198264ffc96758
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2024-03-24 14:46:55 +0300

    Add script: repeat-perf-test.sh (#8760)
    This script allows running the performance test suite against one or more different versions of the codebase.

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -5,7 +5,6 @@
 var watch = require('watch-glob');
 var http_server = require('http-server');
 var debounce = require('lodash.debounce');
-var buildPouchDB = require('./build-pouchdb');
 var browserify = require('browserify');
 var fs = require('fs');
 
@@ -32,6 +31,9 @@ if (process.env.COUCH_HOST) {
 if (process.env.ITERATIONS) {
   queryParams.iterations = process.env.ITERATIONS;
 }
+if (process.env.SRC_ROOT) {
+  queryParams.srcRoot = process.env.SRC_ROOT;
+}
 if (process.env.USE_MINIFIED) {
   queryParams.useMinified = process.env.USE_MINIFIED;
 }
@@ -39,6 +41,7 @@ if (process.env.USE_MINIFIED) {
 var rebuildPromise = Promise.resolve();
 
 function rebuildPouch() {
+  const buildPouchDB = require('./build-pouchdb');
   rebuildPromise = rebuildPromise.then(buildPouchDB).then(function () {
     console.log('Rebuilt packages/node_modules/pouchdb');
   }).catch(console.error);
@@ -85,7 +88,7 @@ function watchAll() {
 var filesWritten = false;
 
 Promise.resolve().then(function () {
-  if (process.env.CI) {
+  if (process.env.CI || process.env.NO_REBUILD) {
     return; // don't bother rebuilding in CI; we already built
   }
   return Promise.all([
