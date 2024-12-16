1b7775782217099a975f535d1a2f70f3869cc827
alxndrsn <alxndrsn>
2024-12-16 14:16:49 +0000

    ci: verify package-lock has no extraneous dependencies listed

---
+++ bin/verify-package-lock.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eu
+
+lockfile=package-lock.json
+
+log() { echo >&2 "[verify-package-lock] $*"; }
+
+log "Pruning dependencies..."
+npm prune
+
+log "Checking for changes to $lockfile..."
+if [[ "$(git status --porcelain -- "$lockfile")" != "" ]]; then
+	git diff -- "$lockfile"
+	log "!!!"
+	log "!!! Unexpected changes to $lockfile found; see above"
+	log "!!!"
+  exit 1
+fi
+
+log "Dependencies verified OK."
