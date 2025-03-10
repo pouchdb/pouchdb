74d3d20327797bb10cb90a456c38ed1493211053
Steven-John Lange <s.lange@hz-deutschland.de>
2025-03-10 11:40:12 +0100

    revert: es6 imports for node.setup
    This reverts commit 936958e43f738a4348f893e4f9fe29aa1b618810.

--- tests/integration/node.setup.js
+++ tests/integration/node.setup.js
@@ -1,13 +1,14 @@
+"use strict";
 // throw an error if any EventEmitter adds too many listeners
-import 'throw-max-listeners-error';
+require('throw-max-listeners-error');
 
-import seedrandom from 'seedrandom';
-const seed = (process.env.SEED || Date.now()) + "";
+var seedrandom = require('seedrandom');
+var seed = (process.env.SEED || Date.now()) + "";
 console.log('Seeded with: ' + seed);
 seedrandom(seed, { global: true });
 
-const testsDir = process.env.TESTS_DIR || './tmp';
-import { exec } from 'node:child_process';
+var testsDir = process.env.TESTS_DIR || './tmp';
+var exec = require('child_process').exec;
 function cleanup() {
   // Remove test databases
   exec('rm -r ' + testsDir);
@@ -16,14 +17,11 @@ exec('mkdir -p ' + testsDir, function () {
   process.on('SIGINT', cleanup);
   process.on('exit', cleanup);
 });
-import utils from './utils.js';
-global.testUtils = utils;
+global.testUtils = require('./utils.js');
 global.PouchDB = testUtils.loadPouchDB();
-import chai from 'chai';
-import chaiAsPromised from "chai-as-promised";
-chai.use(chaiAsPromised);
+var chai = require('chai');
+chai.use(require('chai-as-promised'));
 global.should = chai.should();
 global.assert = chai.assert;
-import fs from 'node:fs';
-global.fs = fs;
+global.fs = require('fs');
 global.fs.mkdirSync('./tmp', { recursive: true });
